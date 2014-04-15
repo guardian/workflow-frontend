47ff4ffa3778bdfe07d98ad05d926d69eb709f92
Ben James <ben.james@guardian.co.uk>
2014-04-15 11:59:24 +0100

    Stuff latest messages into our "database"

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,12 @@
 package controllers
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 import play.api.mvc._
 import play.api.libs.json._
 import models.WorkflowContent
-import lib.AWSWorkflowQueue
+import lib.{Database, AWSWorkflowQueue}
 
 object Application extends Controller {
 
@@ -11,12 +14,16 @@ object Application extends Controller {
     Ok(views.html.index("Hello wor... kflow :)"))
   }
 
-  def consumeFeed = Action {
-    val messages = AWSWorkflowQueue.getMessages(5)
+  def consumeFeed = Action.async {
+    val messages = AWSWorkflowQueue.getMessages(10)
 
     val workflowItems = messages.map(AWSWorkflowQueue.parseMessage(_))
 
-    Ok(workflowItems.toList.toString)
+    Future.traverse(workflowItems) { item =>
+      Database.store.alter(_.updated(item.path, item))
+    }
+
+    Database.store.future.map(items => Ok(items.toString))
   }
 
 }
