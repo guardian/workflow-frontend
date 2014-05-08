dda1be0767a97e044b1675e0a12b32d839109e63
Ben James <ben.james@guardian.co.uk>
2014-05-08 14:51:55 +0100

    Add composerId to model

--- app/models/WorkflowContent.scala
+++ app/models/WorkflowContent.scala
@@ -39,6 +39,7 @@ object Contributor {
 
 
 case class WireStatus(
+  composerId: String,
   contributors: List[Contributor],
   path: String,
   published: Boolean,
@@ -50,6 +51,7 @@ case class WireStatus(
 
 case class WorkflowContent(
   id: UUID,
+  composerId: String,
   path: Option[String],
   workingTitle: Option[String],
   contributors: List[Contributor],
@@ -107,6 +109,7 @@ object WorkflowContent {
   def fromWireStatus(wireStatus: WireStatus): WorkflowContent = {
     WorkflowContent(
       UUID.randomUUID(),
+      wireStatus.composerId,
       Some(wireStatus.path),
       None,
       wireStatus.contributors,
@@ -171,7 +174,8 @@ object WireStatus {
 
   import WorkflowStatus._
   implicit val wireStatusReads: Reads[WireStatus] =
-    ( readContributors ~
+    ((__ \ "content" \ "identifiers" \ "composerId").read[String] ~
+      readContributors ~
       (__ \ "content" \ "identifiers" \ "path").read[String] ~
       (__ \ "published").read[Boolean] ~
       (__ \ "whatChanged").read[String] ~
