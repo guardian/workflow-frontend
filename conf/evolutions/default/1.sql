a3972daa914e92ef7893fd72a67edf34ff47405e
Ben James <ben.james@guardian.co.uk>
2014-05-15 15:09:06 +0100

    Make composer id primary key of content table

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,18 +1,17 @@
+﻿CREATE TABLE CONTENT (
+    composer_id        varchar(32)     primary key
+  , path               varchar(128)    not null
+  , last_modified      timestamp       not null
+  , last_modified_by   varchar(32)
+  , status             varchar(16)     not null
+  , content_type       varchar(16)
+);
+
 CREATE TABLE stub (
     pk                 serial          primary key
   , working_title      varchar(128)    not null
   , section            varchar(128)    not null
   , due                timestamp
   , assign_to          varchar(128)
-);
-
-CREATE TABLE content (
-    pk                 serial          primary key
-  , stub               integer         not null         references stub
-  , composer_id        varchar(32)     unique
-  , path               varchar(128)    not null
-  , last_modified      timestamp       not null
-  , last_modified_by   varchar(32)
-  , status             varchar(16)     not null
-  , content_type       varchar(16)
+  , composer_id        varchar(32)     references content
 );
