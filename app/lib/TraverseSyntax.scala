54dcd47333db4ff1d62abf694e11009c98951069
Ben James <ben.james@guardian.co.uk>
2014-05-19 18:01:11 +0100

    Comment to document traverse use case

--- app/lib/TraverseSyntax.scala
+++ app/lib/TraverseSyntax.scala
@@ -6,6 +6,8 @@ import play.api.libs.functional.Applicative
 object TraverseSyntax {
 
   implicit class TraverseOps[A](as: List[A]) {
+
+    /* List[A] => (A => F[B]) => F[List[B]] */
     def traverse[F[_], B](f: A => F[B])(implicit F: Applicative[F]): F[List[B]] =
       as.foldRight[F[List[B]]](F.pure(Nil)) {
         (a, fbs) =>
