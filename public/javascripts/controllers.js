0e4601c40cd3d8d2f8dcb7e400a68682a4046f84
Ben James <ben.james@guardian.co.uk>
2014-05-21 13:55:55 +0100

    boostrap some Angular.js

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('ContentCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
