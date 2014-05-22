7796783ed27369259cef6818598eba5067abfd6a
lindseydew <lindsey.dew@guardian.co.uk>
2014-05-22 18:13:28 +0100

    continue with date filtering

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,10 +1,12 @@
-define(['angular'], function (angular) {
+define(['angular', 'moment'], function (angular, moment) {
     'use strict';
 
     return angular.module('myApp.controllers', [])
          .controller('ContentCtrl', ['$scope','$http', function($scope, $http) {
 
-
+             function formatDateForUri(date) {
+                 return encodeURIComponent(moment(date).format("YYYY-MM-DDTHH:mm:ssZ"))
+             }
              $scope.stateIsSelected = function(state) {
                  return $scope.selectedState == state;
              }
@@ -14,22 +16,33 @@ define(['angular'], function (angular) {
                  getContent();
              }
 
-            $scope.selectDate = function(date) {
-              
-              $scope.selectedDate = date;
-                   getContent();
-              };
+             $scope.selectDate = function(date) {
+                  if(date=='today') {
+                      $scope.dueFrom = moment().startOf('day');
+                      $scope.dueUntil = moment().startOf('day').add('days', 1);
+                  }
+                  if(date=='tomorrow') {
+                      $scope.dueFrom = moment().startOf('day').add('days', 1);
+                      $scope.dueUntil = moment().startOf('day').add('days', 2);
+                  }
+                 $scope.selectedDate = date;
+                  getContent();
+             };
 
-             function getContent() {
-                 var uri = 'http://localhost:9000/content'
+            function getContent() {
+                 var uri = 'http://localhost:9000/content';
+                 var params = {};
                  if ($scope.selectedState) {
-                     uri = uri + '?state=' + $scope.selectedState;
+                    params.state = $scope.selectedState;
                  }
-                 $http.get(uri).success(function(response){
-                     $scope.contentItems = response.content;
+                 if ($scope.selectedDate) {
+                    params["due.from"] = formatDateForUri($scope.dueFrom);
+                    params["due.until"] = formatDateForUri($scope.dueUntil);
+                 }
+                 $http.get(uri, {params: params}).success(function(response){
+                    $scope.contentItems = response.content;
                  });
-             }
-
+            }
              getContent();
          }])
          .controller('MyCtrl2', ['$scope', function($scope) {
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         'angular': 'components/angular.min',
+         'moment': 'components/moment.min',
         'angularRoute': 'components/angular-route'
     },
     shim: {
@@ -12,8 +13,8 @@ require.config({
 //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require(['angular', 'app', 'routes'],
-    function(angular, app, routes) {
+require(['angular', 'app', 'routes', 'moment'],
+    function(angular, app, route) {
         'use strict';
         angular.element().ready(function() {
             angular.resumeBootstrap([app['name']]);
