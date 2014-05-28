a4728328f34feed5fc2fada630635fe240eaec54
Ben James <ben.james@guardian.co.uk>
2014-05-28 12:37:04 +0100

    Fix display of datetimes

--- app/Global.scala
+++ app/Global.scala
@@ -1,3 +1,4 @@
+import java.util.TimeZone
 import akka.actor.Props
 import play.api.libs.concurrent.Akka
 import play.api.mvc.WithFilters
@@ -6,6 +7,19 @@ import lib.{SectionDatabase, RedirectToHTTPSFilter, SqsReader, ComposerSqsReader
 
 
 object Global extends WithFilters(RedirectToHTTPSFilter) with GlobalSettings {
+
+  override def beforeStart(app: Application) {
+
+    /* It's horrible, but this is absolutely necessary for correct interpretation
+     * of datetime columns in the database which do not have a timezone.
+     * The JDBC driver interprets these using the JVM's default timezone which is
+     * almost certainly what no sane person ever wants to do.
+     */
+
+    System.setProperty("user.timezone", "UTC")
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+  }
+
   override def onStart(app: Application) {
 
     import play.api.Play.current
--- app/lib/database.scala
+++ app/lib/database.scala
@@ -23,16 +23,15 @@ object AnormExtension {
   implicit def rowToDateTime: Column[DateTime] = Column.nonNull { (value, meta) =>
     val MetaDataItem(qualified, nullable, clazz) = meta
     value match {
-      case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime))
-      case d: java.sql.Date => Right(new DateTime(d.getTime))
-      case str: java.lang.String => Right(dateFormatGeneration.parseDateTime(str))
+      case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime, DateTimeZone.UTC))
+      case d: java.sql.Date => Right(new DateTime(d.getTime, DateTimeZone.UTC))
       case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass) )
     }
   }
 
   implicit val dateTimeToStatement = new ToStatement[DateTime] {
     def set(s: java.sql.PreparedStatement, index: Int, aValue: DateTime): Unit = {
-      s.setTimestamp(index, new java.sql.Timestamp(aValue.withMillisOfSecond(0).getMillis()) )
+      s.setTimestamp(index, new java.sql.Timestamp(aValue.withMillisOfSecond(0).getMillis))
     }
   }
 
