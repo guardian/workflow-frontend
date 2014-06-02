185411362c730e8f589cd5ef8494499003fde827
lindseydew <lindsey.dew@guardian.co.uk>
2014-06-02 17:36:26 +0100

    add the ability to add content to composer on new stub modal

--- app/assets/javascripts/controllers/stubs/modal.js
+++ app/assets/javascripts/controllers/stubs/modal.js
@@ -16,12 +16,15 @@ define([
     var StubModalInstanceCtrl = function ($scope, $modalInstance, stub) {
         //default to technology for first pass of testing
         if (stub === undefined) {
-            $scope.stubForm = {'section': 'Technology'};
+            $scope.stubForm = {'section': 'Technology',
+                               'contentType': 'article'
+                              };
         }
         else {
             $scope.stubForm = stub;
         }
-        $scope.ok = function () {
+        $scope.ok = function (addToComposer) {
+            $scope.stubForm.addToComposer = addToComposer;
             $modalInstance.close($scope.stubForm);
         };
         $scope.cancel = function () {
@@ -29,7 +32,7 @@ define([
         };
     };
 
-    stubsControllers.controller('StubModalInstanceCtrl', ['$scope','$modalInstance','items', StubModalInstanceCtrl]);
+    stubsControllers.controller('StubModalInstanceCtrl', ['$scope','$modalInstance','stub', 'addToComposer', StubModalInstanceCtrl]);
 
     stubsControllers.controller('StubModalCtrl', ['$scope', '$modal', '$http', function($scope, $modal, $http){
 
@@ -37,7 +40,7 @@ define([
             $scope.open(stub);
         });
 
-        $scope.open = function (stub) {
+        $scope.open = function (stub, addToComposer) {
 
             var modalInstance = $modal.open({
                 templateUrl: 'stubModalContent.html',
@@ -45,6 +48,9 @@ define([
                 resolve: {
                     stub: function () {
                         return stub;
+                    },
+                    addToComposer: function() {
+                        return addToComposer;
                     }
                 }
             });
@@ -52,25 +58,42 @@ define([
             modalInstance.result.then(function (stub) {
                 var newStub = angular.copy(stub);
                 newStub.due = Date.create(stub.due).toISOString();
-
-                var response;
-                if (stub.id === undefined) {
-                    response = $http({
-                       method: 'POST',
-                       url: '/api/stubs',
-                       params: newStub,
-                       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                   });
+                var updateOrCreateStubResponse = function(stub) {
+                    var response;
+                    if (stub.id === undefined) {
+                        response = $http({
+                            method: 'POST',
+                            url: '/api/stubs',
+                            params: stub,
+                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        });
+                    }
+                    else {
+                        response = $http({
+                            method: 'PUT',
+                            url: '/api/stubs/' + stub.id,
+                            params: stub,
+                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        });
+                    }
+                    return response
                 }
-                else {
-                    response = $http({
-                        method: 'PUT',
-                        url: '/api/stubs/' + stub.id,
-                        params: newStub,
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                var response;
+                if(stub.addToComposer) {
+                    var type = stub.contentType;
+                    $http({
+                        method: 'POST',
+                        url: 'http://localhost:9081/admin/api/content',
+                        params: {'type': type},
+                        withCredentials: true
+                    }).success(function(data){
+                        newStub.composerId = data.data.id;
+                        response = updateOrCreateStubResponse(newStub)
                     });
                 }
-
+                else {
+                    response = updateOrCreateStubResponse(newStub)
+                }
                 response.success(function(){
                     $scope.$emit('getStubs');
                 });
