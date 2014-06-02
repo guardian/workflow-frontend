6e47fdf91c4892bb45b4b61a77d6cfc44642a83f
lindseydew <lindsey.dew@guardian.co.uk>
2014-06-02 11:16:26 +0100

    add angular date picker dependencies

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,9 @@ require.config({
         'moment': 'components/moment.min',
         'angularRoute': 'components/angular-route',
         'uiBootstrap': 'components/ui-bootstrap-tpls-0.11.0.min',
+        'ui.bootstrap.datetimepicker': 'components/datetimepicker',
         'sugar': 'components/sugar.min'
+
     },
     shim: {
         'angular' : {'exports' : 'angular'},
@@ -16,7 +18,7 @@ require.config({
 //http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
 window.name = "NG_DEFER_BOOTSTRAP!";
 
-require(['angular', 'app', 'routes', 'moment', 'uiBootstrap', 'sugar'],
+require(['angular', 'app', 'routes', 'moment', 'uiBootstrap', 'sugar', 'ui.bootstrap.datetimepicker'],
     function(angular, app, route) {
         'use strict';
         angular.element().ready(function() {
