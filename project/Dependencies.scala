c342da615e3fab252f3444748a6ca6235cd8278f
Ben James <ben.james@guardian.co.uk>
2014-06-18 16:52:10 +0100

    Pull out dependencies into a new module

---
+++ project/Dependencies.scala
@@ -0,0 +1,17 @@
+import sbt._
+import play.Keys.jdbc
+
+object Dependencies {
+
+  val databaseDependencies = Seq(
+    jdbc,
+    "com.typesafe.play" %% "play-slick" % "0.6.0.1",
+    "com.github.tototoshi" %% "slick-joda-mapper" % "1.1.0",
+    "org.postgresql" % "postgresql" % "9.3-1100-jdbc4"
+  )
+
+  val awsDependencies = Seq("com.amazonaws" % "aws-java-sdk" % "1.7.5")
+
+  val akkaDependencies = Seq("com.typesafe.akka" %% "akka-agent" % "2.2.0")
+
+}
--- project/WorkflowBuild.scala
+++ project/WorkflowBuild.scala
@@ -4,7 +4,7 @@ import play.Keys._
 import plugins.PlayArtifact._
 import sbtassembly.Plugin.{AssemblyKeys, MergeStrategy}
 import AssemblyKeys._
-import play.Keys.jdbc
+import Dependencies._
 
 
 object WorkflowBuild extends Build {
@@ -21,17 +21,13 @@ object WorkflowBuild extends Build {
 
   val root = playProject("prototype")
     .settings(
-      libraryDependencies ++= Seq(
-        "com.amazonaws" % "aws-java-sdk" % "1.7.5",
-        "com.typesafe.akka" %% "akka-agent" % "2.2.0",
-        jdbc,
-        "com.typesafe.play" %% "play-slick" % "0.6.0.1",
-        "com.github.tototoshi" %% "slick-joda-mapper" % "1.1.0",
-        "org.postgresql" % "postgresql" % "9.3-1100-jdbc4"
-      ),
+      libraryDependencies ++= databaseDependencies ++ akkaDependencies ++ awsDependencies,
       requireJs += "main.js",
       requireJsShim += "main.js"
     )
+
+  def project(path: String): Project =
+    Project(path, file(path)).settings(commonSettings: _*)
 
   def playProject(path: String): Project =
     play.Project(path, path = file(path))
