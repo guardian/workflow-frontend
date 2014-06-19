43d40e4fd78a8a99ac9414201198171614768e30
Ben James <ben.james@guardian.co.uk>
2014-06-19 12:16:59 +0100

    Consumption of notifications by dashboard can be disabled

--- app/Global.scala
+++ app/Global.scala
@@ -2,7 +2,7 @@ import java.util.TimeZone
 import akka.actor.Props
 import play.api.libs.concurrent.Akka
 import play.api.mvc.WithFilters
-import play.api.{GlobalSettings, Application}
+import play.api.{Logger, GlobalSettings, Application}
 import lib.{SectionDatabase, RedirectToHTTPSFilter, SqsReader, ComposerSqsReader}
 
 
@@ -25,12 +25,21 @@ object Global extends WithFilters(RedirectToHTTPSFilter) with GlobalSettings {
     import play.api.Play.current
     import play.api.libs.concurrent.Execution.Implicits._
     import scala.concurrent.duration._
-    Akka.system.scheduler.schedule(
-      initialDelay = 0.seconds,
-      interval = 3.seconds,
-      receiver = Akka.system.actorOf(Props[ComposerSqsReader]),
-      message = SqsReader
-    )
+
+    val config = play.api.Play.configuration
+
+    if (config.getBoolean("consumeNotifications.active").getOrElse(true)) {
+      Logger.info("Started in ACTIVE mode. Consuming notifications...")
+      Akka.system.scheduler.schedule(
+        initialDelay = 0.seconds,
+        interval = 3.seconds,
+        receiver = Akka.system.actorOf(Props[ComposerSqsReader]),
+        message = SqsReader
+      )
+    }
+    else {
+      Logger.info("Started in PASSIVE mode. Not consuming notifications.")
+    }
 
     // prod this to make it load initial state.
     SectionDatabase.sectionList
