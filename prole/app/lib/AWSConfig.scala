13869f792b47ed0bcf730cf7264a2c5747349747
Ben James <ben.james@guardian.co.uk>
2014-06-20 17:44:29 +0100

    Revert "Merge pull request #66 from guardian/ld-improve-sqs-reader"
    This reverts commit 8f53434eaa5e5f28c2d38ce3d16193fec330edb2, reversing
    changes made to ad305af2702363385a0f6e5b6db54863c9045576.

--- prole/app/lib/AWSConfig.scala
+++ prole/app/lib/AWSConfig.scala
@@ -43,6 +43,7 @@ object AWSWorkflowQueue {
     )
   }
 
+  //todo validate the json
   def toWireStatus(awsMsg: Message): JsResult[WireStatus] = {
     val body = Json.parse(awsMsg.getBody)
     (body \ "Message").validate[String].flatMap { msg =>
