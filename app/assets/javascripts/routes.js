8cc5866bdd16c51d308541eca789279f554767cc
James Pamplin <james.pamplin@guardian.co.uk>
2014-06-25 14:43:54 +0000

    Moved routes and main module dependencies into app.js

--- app/assets/javascripts/routes.js
+++
@@ -1,9 +0,0 @@
-define(['angular', 'app'], function(angular, app) {
-
-    'use strict';
-
-    return app.config(['$routeProvider', function($routeProvider) {
-             $routeProvider.when('/dashboard', {templateUrl: 'dashboard', controller: 'DashboardCtrl'});
-             $routeProvider.otherwise({redirectTo: '/dashboard'});
-          }]);
-});
