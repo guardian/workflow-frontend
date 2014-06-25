5d0710994240c040e34e1509471f3df3eb57abf0
James Pamplin <james.pamplin@guardian.co.uk>
2014-06-25 14:43:54 +0000

    Moved routes and main module dependencies into app.js

--- prototype/app/assets/javascripts/app.js
+++ prototype/app/assets/javascripts/app.js
@@ -3,9 +3,8 @@ define([
     'filters',
     'services',
     'directives',
-    'config',
+    'wfconfig',
     'controllers',
-    'xeditable',
 
     'controllers/dashboard',
     'controllers/dashboard/content-item',
@@ -16,28 +15,40 @@ define([
     'services/sections-service',
     'services/composer-service',
 
+    'xeditable',
     'angularRoute'
+
     ], function (angular, filters, services, directives, config, controllers) {
 
         'use strict';
 
         var app =  angular.module('workflow', [
-            'ngRoute',
-            'dashboardControllers',
-            'workflow.filters',
-            'workflow.services',
-            'workflow.directives',
-            'workflow.config',
-            'workflow.controllers',
-            'sectionsService',
-            'composerService',
-            'xeditable'
+          'ngRoute',
+          'dashboardControllers',
+          'workflow.filters',
+          'workflow.services',
+          'workflow.directives',
+          'workflow.config',
+          'workflow.controllers',
+          'sectionsService',
+          'composerService',
+          'xeditable'
         ]);
+
+        // App routes
+        app.config(['$routeProvider', function($routeProvider) {
+          $routeProvider.when('/dashboard', { templateUrl: 'dashboard', controller: 'DashboardCtrl' });
+          $routeProvider.otherwise({redirectTo: '/dashboard'});
+        }]);
 
         app.run(function(editableOptions) {
             editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
         });
-    
+
+        // delayed bootstrap for async page loading
+        // angular.bootstrap(document, ['workflow']);
+        angular.resumeBootstrap(['workflow']);
+
         return app;
 
 });
