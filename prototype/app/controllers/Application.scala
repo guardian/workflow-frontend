d89dcc3aaade2ec4fc197087aa7e5c026bb3ad8c
lindseydew <lindsey.dew@guardian.co.uk>
2014-06-25 12:52:16 +0100

    remove openID authentication

--- prototype/app/controllers/Application.scala
+++ prototype/app/controllers/Application.scala
@@ -8,49 +8,19 @@ import com.gu.workflow.syntax.RequestSyntax._
 import play.api.mvc._
 import play.api.libs.json.Json
 import play.api.libs.openid.OpenID
+import com.gu.googleauth.{AuthenticatedRequest, UserIdentity}
 
 
-object Application extends Controller with Authenticated {
+object Application extends Controller with AuthActions {
 
-  def index = Authenticated.async {
+  def index = AuthAction.async { request =>
     for {
       statuses <- StatusDatabase.statuses
     }
     yield Ok(views.html.index(Json.obj("data" -> statuses)))
   }
 
-  def login = Action {
-    Ok(views.html.login())
-  }
-
-  def loginPost = Action.async { implicit req =>
-    val openIdAttributes = Seq(
-      ("email", "http://axschema.org/contact/email"),
-      ("firstname", "http://axschema.org/namePerson/first"),
-      ("lastname", "http://axschema.org/namePerson/last")
-    )
-    val googleOpenIdUrl = "https://www.google.com/accounts/o8/id"
-    val redirectTo = routes.Application.openIdRedirect.absoluteURL(secure = req.isSecure)
-    OpenID.redirectURL(googleOpenIdUrl, redirectTo, openIdAttributes)
-    .map(Redirect(_))
-  }
-
-  def openIdRedirect = Action.async { implicit req =>
-    OpenID.verifiedId.map { userInfo =>
-      val attr = userInfo.attributes
-      val user = for { email <- attr.get("email")
-                      if email.endsWith("@guardian.co.uk") || email.endsWith("@theguardian.com")
-                      firstName <- attr.get("firstname")
-                      lastName <- attr.get("lastname")
-                     } yield User(userInfo.id, email, firstName, lastName)
-
-      user.map {
-        u => Redirect(routes.Application.index).withSession("user" -> Json.toJson(u).toString)
-      }.getOrElse(Redirect(routes.Application.login()))
-    }
-  }
-
-  def dashboard = Authenticated.async { req =>
+  def dashboard = AuthAction.async { req =>
     for {
       sections <- SectionDatabase.sectionList
       statuses <- StatusDatabase.statuses
