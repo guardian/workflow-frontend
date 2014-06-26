5c5c748fed3b1e7f9439999c4f0d432bce325083
James Pamplin <james.pamplin@guardian.co.uk>
2014-06-26 16:16:05 +0000

    Fixed package config to properly work locally

--- prototype/public/config.js
+++ prototype/public/config.js
@@ -1,4 +1,5 @@
 System.config({
+  "baseURL": "./assets",
   "paths": {
     "*": "*.js",
     "npm:*": "jspm_packages/npm/*.js",
@@ -8,29 +9,49 @@ System.config({
 
 System.config({
   "map": {
-    "npm:angular-xeditable": "npm:angular-xeditable@^0.0.1",
-    "npm:bootstrap": "npm:bootstrap@^3.1.1",
-    "npm:angular-bootstrap": "npm:angular-bootstrap@^0.11.0",
-    "npm:moment": "npm:moment@^2.7.0",
-    "npm:angular": "npm:angular@^1.2.18",
-    "npm:underscore": "npm:underscore@^1.6.0",
-    "npm:sugar": "npm:sugar@^1.4.0",
-    "github:components/angular-route": "github:components/angular-route@^1.2.0",
-    "github:dalelotts/angular-bootstrap-datetimepicker": "github:dalelotts/angular-bootstrap-datetimepicker@^0.2.0"
+    "app": "javascripts/app",
+    "uiBootstrap": "npm:angular-bootstrap@^0.11.0",
+    "ui.bootstrap.datetimepicker": "github:dalelotts/angular-bootstrap-datetimepicker@^0.2.4",
+    "bootstrapJs": "github:components/bootstrap@^3.1.1",
+    "angularRoute": "github:components/angular-route@^1.2.0",
+    "xeditable": "github:vitalets/angular-xeditable@^0.1.8",
+    "underscore": "github:jashkenas/underscore@^1.6.0",
+    "sugar": "npm:sugar@^1.4.1",
+    "moment": "github:moment/moment@^2.7.0",
+    "angular": "github:angular/bower-angular@1.2.1",
+    "npm:angular-bootstrap@0.11.0": {},
+    "npm:sugar@1.4.1": {},
+    "github:jspm/nodelibs@0.0.2": {
+      "base64-js": "npm:base64-js@^0.0.4",
+      "ieee754": "npm:ieee754@^1.1.1",
+      "Base64": "npm:Base64@0.2",
+      "json": "github:systemjs/plugin-json@master",
+      "inherits": "npm:inherits@^2.0.1"
+    },
+    "npm:base64-js@0.0.4": {},
+    "npm:ieee754@1.1.3": {},
+    "npm:Base64@0.2.1": {},
+    "npm:inherits@2.0.1": {}
   }
 });
 
 System.config({
   "versions": {
-    "npm:angular-xeditable": "0.0.1",
-    "npm:bootstrap": "3.1.1",
     "npm:angular-bootstrap": "0.11.0",
-    "npm:moment": "2.7.0",
-    "npm:angular": "1.2.18",
-    "npm:underscore": "1.6.0",
-    "npm:sugar": "1.4.1",
+    "github:dalelotts/angular-bootstrap-datetimepicker": "0.2.4",
+    "github:components/bootstrap": "3.1.1",
     "github:components/angular-route": "1.2.0",
-    "github:dalelotts/angular-bootstrap-datetimepicker": "0.2.4"
+    "github:vitalets/angular-xeditable": "0.1.8",
+    "github:jashkenas/underscore": "1.6.0",
+    "npm:sugar": "1.4.1",
+    "github:moment/moment": "2.7.0",
+    "github:angular/bower-angular": "1.2.1",
+    "github:jspm/nodelibs": "0.0.2",
+    "npm:base64-js": "0.0.4",
+    "npm:ieee754": "1.1.3",
+    "npm:Base64": "0.2.1",
+    "github:systemjs/plugin-json": "master",
+    "npm:inherits": "2.0.1"
   }
 });
 
