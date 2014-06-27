88adc871897594a2a87d386c36b4d782e0b4cce7
lindseydew <lindsey.dew@guardian.co.uk>
2014-06-27 12:25:19 +0100

    make the application secret dependent on starting prototype

--- common-lib/src/main/scala/com/gu/workflow/lib/Config.scala
+++ common-lib/src/main/scala/com/gu/workflow/lib/Config.scala
@@ -6,6 +6,7 @@ object Config {
 
   def getConfigString(name: String): Either[String, String] = {
     config.getString(name) match {
+      case Some("") => Left(s"empty string set for ${name}")
       case Some(value) => Right(value)
       case None => Left(s"could not find ${name}")
     }
