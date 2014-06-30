a13b3390ef21d22106534328fa56f640c0ddffa7
James Pamplin <james.pamplin@guardian.co.uk>
2014-06-30 09:51:16 +0000

    Removed xeditable manual override as added to jspm registry

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ npm install
 pushd public
 
   echo "Installing jspm client-side packages"
-  ## Temp override of xeditable
-  ../node_modules/.bin/jspm install xeditable=github:vitalets/angular-xeditable -o "{main: 'dist/js/xeditable.min'}"
   ../node_modules/.bin/jspm install
 
 popd
