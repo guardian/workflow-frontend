40ac6c203b666ba0a8be702dee2dab5378711689
Stephen Wells <stephen.wells@guardian.co.uk>
2014-07-04 11:15:21 +0100

    build should fail if npm etc. fails

--- prototype/setup.sh
+++ prototype/setup.sh
@@ -2,10 +2,23 @@
 
 echo "Installing nodejs packages"
 npm install
+NPM_EXIT=$?
+
+if [ $JSPM_EXIT != "0" ]; then
+    exit 1
+fi
 
 pushd public
 
   echo "Installing jspm client-side packages"
   ../node_modules/.bin/jspm install
 
+  JSPM_EXIT=$?
 popd
+
+
+if [ $JSPM_EXIT == "0" ]; then
+    exit 0
+else
+    exit 1
+fi
