694972bf81a50de179e7454c31bfb8c3fd4a3c58
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-21 14:35:57 +0000

    Added settings service which uses localStorage for persistence

---
+++ public/lib/settings-service.js
@@ -0,0 +1,42 @@
+/**
+ * Module containing a service providing User settings stored locally.
+ */
+
+import './local-storage-adapter';
+
+angular.module('wfSettingsService', ['wfLocalStorageAdapter'])
+  .factory('wfSettingsService', ['wfLocalStorageAdapter', function(localStorageAdapter) {
+
+    class Settings {
+
+      constructor() {
+        this._settings = localStorageAdapter.getObject('settings');
+        if (!this._settings) {
+          this.setDefaults();
+        }
+
+        // TODO: add listeners to local storage events
+      }
+
+      setDefaults() {
+        this._settings = {
+          timezone: 'UK/London'
+        };
+        localStorageAdapter.set('settings', this._settings);
+      }
+
+      get(key) {
+        return this._settings[key];
+      }
+
+      set(key, value) {
+        this._settings[key] = value;
+        localStorageAdapter.set('settings', this._settings);
+        return this;
+      }
+
+    }
+
+    return new Settings();
+
+  }]);
