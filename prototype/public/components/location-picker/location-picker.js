c048b47667e041a16cca68eaee176b172a32afdc
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-23 15:10:49 +0000

    Added localtimes to location picker dropdown

--- prototype/public/components/location-picker/location-picker.js
+++ prototype/public/components/location-picker/location-picker.js
@@ -1,13 +1,43 @@
+/**
+ * Controller for location picker in top-toolbar.
+ */
 
 import angular from 'angular';
 
 import 'lib/date-service';
 
+import './location-picker.css!';
+
 angular.module('wfLocationPicker', ['wfDateService'])
-  .controller('wfLocationPickerController', ['$scope', 'wfDateService', function($scope, dateService) {
+  .controller('wfLocationPickerController', ['$scope', '$timeout', 'wfDateService', function($scope, $timeout, dateService) {
     this.locations = dateService.timezones;
     this.locationKey = dateService.getCurrentTimezoneKey();
 
+    // Setup timer to update current time every minute
+    var timer;
+
+    function msTilNextMinute() {
+      return 60000 - (Date.now() % 60000);
+    }
+
+    function incrementTime() {
+      this.now = Date.now();
+
+      timer = $timeout(angular.bind(this, incrementTime), msTilNextMinute());
+    }
+
+    // initialise timer
+    incrementTime.call(this);
+
+    // Cancel timer when scope is destroyed
+    $scope.$on('$destroy', function() {
+      if (timer) {
+        $timeout.cancel(timer);
+      }
+    });
+
+
+    // Watch for changes to selected location
     $scope.$watch(
       angular.bind(this, function() { return this.locationKey; }),
       function(newValue, oldValue) {
