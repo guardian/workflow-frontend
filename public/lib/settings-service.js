3290423b9fe077cc20d2df7f46320cd18a472631
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-24 14:12:16 +0000

    Added some extra error checking to settings and localstorage

--- public/lib/settings-service.js
+++ public/lib/settings-service.js
@@ -2,15 +2,23 @@
  * Module containing a service providing User settings stored locally.
  */
 
+import angular from 'angular';
+
 import './local-storage-adapter';
 
 angular.module('wfSettingsService', ['wfLocalStorageAdapter'])
-  .factory('wfSettingsService', ['wfLocalStorageAdapter', function(localStorageAdapter) {
+  .factory('wfSettingsService', ['$log', 'wfLocalStorageAdapter', function($log, localStorageAdapter) {
 
     class Settings {
 
       constructor() {
-        this._settings = localStorageAdapter.getObject('settings') || {};
+        try {
+          this._settings = localStorageAdapter.getObject('settings') || {};
+
+        } catch (err) {
+          $log.error('Could not retrieve settings from localStorage: ' + err);
+          this._settings = {};
+        }
 
         // TODO: add listeners to local storage events
       }
@@ -19,9 +27,20 @@ angular.module('wfSettingsService', ['wfLocalStorageAdapter'])
         return this._settings[key];
       }
 
+      /**
+       * Set a setting. Persists in browser localStorage.
+       */
       set(key, value) {
         this._settings[key] = value;
-        localStorageAdapter.set('settings', this._settings);
+
+        try {
+          localStorageAdapter.set('settings', this._settings);
+
+        } catch (err) {
+          // Silently fail on local storage error
+          $log.error('Error setting value in localStorage: ' + err);
+        }
+
         return this;
       }
 
