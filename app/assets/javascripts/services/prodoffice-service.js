17f0a30345f61946fbcbb070df461ef30e310d3b
Paul Roberts <paul.roberts@guardian.co.uk>
2014-07-28 13:18:06 +0100

    use the location service to set the default prod office

--- app/assets/javascripts/services/prodoffice-service.js
+++ app/assets/javascripts/services/prodoffice-service.js
@@ -1,10 +1,28 @@
 define(['angular'], function(angular) {
     'use strict';
 
-    var prodOfficeService = angular.module('prodOfficeService', []);
+    var prodOfficeService = angular.module('prodOfficeService', ['wfLocationService']);
 
     prodOfficeService.factory('prodOfficeService',
-        [function() {
+        ['wfLocationService', function(wfLocationService) {
+
+            /* mapping from city (as returned by getLocationKey()) to
+             * office */
+            var tz_office = {
+                "NYC"     : "US",
+                "SYD"     : "AU",
+                "LON"     : "UK",
+                "default" : "UK"
+            }
+
+            function officeToTimezone(office) {
+                if (office && tz_office[office]) return tz_office[office]
+                else return tz_office["default"];
+            }
+
+            function defaultOffice() {
+                return officeToTimezone(wfLocationService.getLocationKey());
+            }
 
             function getProdOffices() {
                 return [
@@ -15,7 +33,8 @@ define(['angular'], function(angular) {
             };
 
             return {
-                getProdOffices: getProdOffices
+                getProdOffices: getProdOffices,
+                defaultOffice:  defaultOffice()
             };
 
         }]);
