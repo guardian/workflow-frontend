34d523db27c9903c111ec783a0b52708abbef673
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-29 14:14:22 +0000

    Basic mixpanel integration

---
+++ public/lib/user.js
@@ -0,0 +1,19 @@
+/**
+ * User Module. Provides the "wfUser" service containing data fields for the
+ * current signed in user.
+ *
+ * @example
+ * angular.module('myModule', 'wfUser')
+ *   .controller('myController', ['wfUser', function(wfUser) {
+ *     var email = wfUser.email;
+ *     var lastName = wfUser.lastName;
+ *     var firstName = wfUser.firstName;
+ *   }]);
+ */
+
+import angular from 'angular';
+
+angular.module('wfUser', [])
+  .factory('wfUser', [function() {
+    return window._wfConfig.user;
+  }]);
