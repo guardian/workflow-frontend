b519e348383895c9eacbff29d1e4b3939c584b9e
lindseydew <lindsey.dew@guardian.co.uk>
2014-08-03 18:10:06 +0100

    use an event driven model for tracking changes to getcontent

--- public/lib/filters-service.js
+++ public/lib/filters-service.js
@@ -6,28 +6,32 @@ import './format-service';
 import './date-filters-service';
 
 angular.module('filtersService', ['urlService', 'formatService', 'dateFilters'])
-       .factory('filtersService', function(urlService, formatService, dateFilters){
+       .factory('filtersService', function(urlService, formatService, dateFilters) {
 
-        class FiltersService {
+        class FiltersService
+        {
 
-            constructor() {
+            constructor()
+            {
                 this.filters = {};
                 this.filters['status'] = urlService.get('status');
-                this.filters['state']  = urlService.get('state');
-                this.filters['section']= urlService.get('section');
-                this.filters['content-type']= urlService.get('content-type');
+                this.filters['state'] = urlService.get('state');
+                this.filters['section'] = urlService.get('section');
+                this.filters['content-type'] = urlService.get('content-type');
                 this.filters['selectedDate'] = formatService.stringToDate(urlService.get('selectedDate'));
                 this.filters['flags'] = formatService.stringToArray(urlService.get('flags'));
                 this.filters['prodOffice'] = urlService.get('prodOffice');
             }
 
-            toServerParams() {
+            toServerParams()
+            {
                 var params = {};
                 params.status = this.filters['status'];
                 params.state = this.filters['state'];
                 params.section = this.filters['section'];
                 params["content-type"] = this.filters["content-type"];
                 params.flags = this.filters['flags'];
+                params.prodOffice = this.filters['prodOffice'];
 
                 var dateParams = dateFilters.setToAndFrom(this.filters['selectedDate']);
 
@@ -39,15 +43,54 @@ angular.module('filtersService', ['urlService', 'formatService', 'dateFilters'])
 
             update(key, value) {
                 this.filters[key] = value;
-                urlService.set(key,formatService.objToStr(value));
+                urlService.set(key, formatService.objToStr(value));
             }
 
-            get(key) {
+            get(key){
                 return this.filters[key];
             }
-
         }
 
         return new FiltersService();
 
-    });
+
+    })
+
+    .run(['$rootScope', 'filtersService', function($rootScope, filtersService){
+
+        $rootScope.$on('filtersChanged.prodOffice', function(event, data) {
+            filtersService.update('prodOffice', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.content-type', function(event, data) {
+            filtersService.update('content-type', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.section', function(event, data) {
+            filtersService.update('section', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.state', function(event, data) {
+            filtersService.update('state', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.status', function(event, data) {
+            filtersService.update('status', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.flags', function(event, data) {
+            filtersService.update('flags', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+        $rootScope.$on('filtersChanged.selectedDate', function(event, data) {
+            filtersService.update('selectedDate', data);
+            $rootScope.$broadcast('getContent');
+        });
+
+    }]);
