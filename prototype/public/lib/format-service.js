e0fa76e020592c1a210d46da937f0856a5f121e1
lindseydew <lindsey.dew@guardian.co.uk>
2014-08-03 15:21:14 +0100

    use a filters service to maintain url state

--- prototype/public/lib/format-service.js
+++ prototype/public/lib/format-service.js
@@ -10,17 +10,13 @@ angular.module('formatService',[]).factory('formatService', [function(){
             else return [];
         }
 
-        arrayToString(value) {
-            if(value.length > 0){
-                return value.toString
-            }
-        }
-
         stringToDate(value) {
             if (moment(value, ["DD-MM-YYYY"]).isValid()){
                 return moment(value, ["DD-MM-YYYY"]);
             }
-            else return value;
+            else if(value==='today' || value==='tomorrow' || value==='weekend') {
+                return value;
+            }
         }
 
         dateToString(value) {
@@ -29,6 +25,21 @@ angular.module('formatService',[]).factory('formatService', [function(){
             }
             else return value;
         }
+
+        objToStr(value) {
+            if(typeof value == 'object' && moment.isMoment(value)) {
+                return moment(value).format("DD-MM-YYYY");
+            }
+            else return value;
+        }
+
+        dateForUri(date) {
+            if(moment.isMoment(date)) {
+                return moment(date).format("YYYY-MM-DDTHH:mm:ssZ");
+            }
+            else return date;
+        }
+
     }
 
     return new FormatService();
