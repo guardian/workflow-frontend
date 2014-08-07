ad6e845351e05047664bca0688974b5b360d59d4
James Pamplin <james.pamplin@guardian.co.uk>
2014-08-07 13:35:05 +0000

    Added Karma configuration for Javascript unit tests

---
+++ public/karma.bootstrap.js
@@ -0,0 +1,18 @@
+/**
+ * Karma bootstrap.
+ *
+ * Loads in angular-mocks via System.js (jspm) before tests load to ensure
+ * Mocking libs are available globally for all test specs.
+ */
+
+before(function(done) {
+  System.import('angular-mocks')
+
+    .then(function() {
+      console.log('Bootstrapped Angular mocks');
+
+      done();
+
+    }).catch(done);
+
+});
