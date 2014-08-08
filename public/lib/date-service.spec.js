abc2324bd440ddedf75fe85f38b6ac1d61eac788
James Pamplin <james.pamplin@guardian.co.uk>
2014-08-08 16:21:50 +0000

    Revised date parsing logic to be less coupled to locationService, integrated with Date Picker

--- public/lib/date-service.spec.js
+++ public/lib/date-service.spec.js
@@ -13,30 +13,7 @@ describe('lib/date-service', function() {
 
 
   beforeEach(function() {
-    angular.mock.module('wfDateService', function($provide) {
-
-      // Stub out location
-      $provide.value('wfLocationService', {
-
-        // Default stub location to London
-        getLocationKey: sinon.stub().returns('LON')
-
-      });
-    });
-  });
-
-
-
-  it('can mock location', function() {
-
-    angular.mock.inject(function(wfDateService, wfLocationService) {
-
-      // Mock Sydney location
-      wfLocationService.getLocationKey.returns('SYD');
-
-      var timezone = wfDateService.getTimezone();
-      expect(timezone.tzKey).to.eql('Australia/Sydney');
-    });
+    angular.mock.module('wfDateService');
   });
 
 
@@ -44,11 +21,8 @@ describe('lib/date-service', function() {
 
     angular.mock.inject(function(wfDateService, wfLocationService) {
 
-      // Mock London location
-      wfLocationService.getLocationKey.returns('LON');
-
       // 6am on 14 August 2014 in London local time (BST +0100)
-      var parsed = wfDateService.parse('14-08-2014 6:00');
+      var parsed = wfDateService.parseDate('14-08-2014 6:00', 'LON');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-14T05:00:00.000Z');
@@ -60,11 +34,8 @@ describe('lib/date-service', function() {
 
     angular.mock.inject(function(wfDateService, wfLocationService) {
 
-      // Mock New York location
-      wfLocationService.getLocationKey.returns('NYC');
-
       // 6am on 14 August 2014 in New York local time (EDT -0400)
-      var parsed = wfDateService.parse('08-14-2014 6:00');
+      var parsed = wfDateService.parseDate('08-14-2014 6:00', 'NYC');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-14T10:00:00.000Z');
@@ -76,11 +47,8 @@ describe('lib/date-service', function() {
 
     angular.mock.inject(function(wfDateService, wfLocationService) {
 
-      // Mock Sydney location
-      wfLocationService.getLocationKey.returns('SYD');
-
       // 6am on 14 August 2014 in Sydney local time (EST +1000)
-      var parsed = wfDateService.parse('14-08-2014 6am');
+      var parsed = wfDateService.parseDate('14-08-2014 6am', 'SYD');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-13T20:00:00.000Z');
