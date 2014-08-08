d2eff076540626d8ad530bb450b432564e54d77c
James Pamplin <james.pamplin@guardian.co.uk>
2014-08-08 16:32:14 +0000

    Renamed service to date parser

--- prototype/public/lib/date-service.spec.js
+++ prototype/public/lib/date-service.spec.js
@@ -19,10 +19,10 @@ describe('lib/date-service', function() {
 
   it('should parse text inputs relative to timezone when in LON', function() {
 
-    angular.mock.inject(function(wfDateService, wfLocationService) {
+    angular.mock.inject(function(wfDateParser) {
 
       // 6am on 14 August 2014 in London local time (BST +0100)
-      var parsed = wfDateService.parseDate('14-08-2014 6:00', 'LON');
+      var parsed = wfDateParser.parseDate('14-08-2014 6:00', 'LON');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-14T05:00:00.000Z');
@@ -32,10 +32,10 @@ describe('lib/date-service', function() {
 
   it('should parse text inputs relative to timezone when in NYC', function() {
 
-    angular.mock.inject(function(wfDateService, wfLocationService) {
+    angular.mock.inject(function(wfDateParser) {
 
       // 6am on 14 August 2014 in New York local time (EDT -0400)
-      var parsed = wfDateService.parseDate('08-14-2014 6:00', 'NYC');
+      var parsed = wfDateParser.parseDate('08-14-2014 6:00', 'NYC');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-14T10:00:00.000Z');
@@ -45,10 +45,10 @@ describe('lib/date-service', function() {
 
   it('should parse text inputs relative to timezone when in SYD', function() {
 
-    angular.mock.inject(function(wfDateService, wfLocationService) {
+    angular.mock.inject(function(wfDateParser) {
 
       // 6am on 14 August 2014 in Sydney local time (EST +1000)
-      var parsed = wfDateService.parseDate('14-08-2014 6am', 'SYD');
+      var parsed = wfDateParser.parseDate('14-08-2014 6am', 'SYD');
 
       expect(parsed).to.be.an.instanceof(Date);
       expect(parsed.toISOString()).to.eql('2014-08-13T20:00:00.000Z');
