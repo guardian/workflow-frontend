8f8d24d7522fd4209599fcea1c1c6a0acd6d2636
Paul Roberts <paul.roberts@guardian.co.uk>
2014-08-28 15:00:52 +0100

    refactor the common code for the created-at filter callbacks

--- prototype/app/assets/javascripts/controllers/dashboard/created-at-filter.js
+++ prototype/app/assets/javascripts/controllers/dashboard/created-at-filter.js
@@ -4,10 +4,15 @@ define(['moment', '../dashboard'], function(moment, dashboardControllers) {
 
   var baseDate = function() { return moment() };
 
+  /* most filters just subtract a certain amount from the base date */
+  function simpleSubtract(unit, amt) {
+    return function(dt) { return { from: dt.subtract(unit, amt) } }
+  }
+
   dashboardControllers.controller('CreatedAtFilter', ['$scope', function($scope) {
     /* cb's return an obj containing from and to. Times relative to the base date */
     $scope.cannedFilters = [{
-      name: "Last 24hrs", cb: function(dt) { return { from: dt.subtract('days', 1) } }
+      name: "Last 24hrs", cb: simpleSubtract('days', 1)
     }]
   }]);
 
