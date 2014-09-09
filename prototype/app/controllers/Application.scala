6cfcb7802906d8dd6830bd29a9c1ae7b0cac1052
James Pamplin <james.pamplin@guardian.co.uk>
2014-09-09 15:55:50 +0000

    Removed legacy files and routes now deprecated

--- prototype/app/controllers/Application.scala
+++ prototype/app/controllers/Application.scala
@@ -14,28 +14,7 @@ import play.api.libs.json.Json
 
 object Application extends Controller with AuthActions {
 
-  def index = AuthAction.async { request =>
-    val user = request.session.get("identity")
-
-    for {
-      statuses <- StatusDatabase.statuses
-      sections = SectionDB.sectionList
-    }
-    //put these in one object
-    yield {
-      Ok(views.html.index(Json.obj("data" -> statuses), Json.obj("data" -> sections), user))
-    }
-  }
-
-  def dashboard = AuthAction.async { req =>
-    for {
-      statuses <- StatusDatabase.statuses
-    }
-    yield {
-      val sections = SectionDB.sectionList
-      Ok(views.html.dashboard(sections, statuses))
-    }
-  }
+  def index = all("Dashboard")
 
   def all(title: String) = AuthAction.async { request =>
 
