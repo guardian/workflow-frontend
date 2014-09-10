a52a5386a25ec1262e4f9ba1187739b59f46419d
James Pamplin <james.pamplin@guardian.co.uk>
2014-09-10 12:20:27 +0000

    Styles and template updated for Login page

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -1,8 +1,9 @@
 package controllers
 
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonNullFormatVisitor
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsNull, JsValue, Json}
 import scala.concurrent.Future
 import com.gu.googleauth._
 import lib.PrototypeConfiguration
@@ -51,9 +52,15 @@ object Login extends Controller with AuthActions {
     )
 
   // this is the only place we use LoginAuthAction - to prevent authentication redirect loops
-  def login = LoginAuthAction { request =>
+  def login = LoginAuthAction { implicit request =>
     val error = request.flash.get("error")
-    Ok(views.html.login(error))
+
+    val user = request.identity match {
+      case None => JsNull
+      case _ => Json.toJson(request.identity.get)
+    }
+    val config = Json.obj("user" -> user)
+    Ok(views.html.login(error, config))
   }
 
   /*
