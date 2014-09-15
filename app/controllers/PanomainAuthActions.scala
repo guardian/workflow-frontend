1d64d26bb4286ae577685350e2b17266cf7cc0e0
Stephen Wells <stephen.wells@guardian.co.uk>
2014-09-15 16:28:29 +0100

    Adds ability to logout.
    although why you'd ever want to leave is beyond me.

--- app/controllers/PanomainAuthActions.scala
+++ app/controllers/PanomainAuthActions.scala
@@ -2,8 +2,10 @@ package controllers
 
 import com.gu.pandomainauth.action.AuthActions
 import com.gu.pandomainauth.model.AuthenticatedUser
+import play.api.Logger
+import play.api.mvc._
 
-trait PanDomainAuthActions extends AuthActions {
+trait PanDomainAuthActions extends AuthActions with Results {
 
   import play.api.Play.current
   lazy val config = play.api.Play.configuration
@@ -13,6 +15,11 @@ trait PanDomainAuthActions extends AuthActions {
   }
 
   override def authCallbackUrl: String = config.getString("host").get + "/oauthCallback"
+
+  override def showUnauthedMessage(message: String)(implicit request: RequestHeader): Result = {
+    Logger.info(message)
+    Ok(views.html.login(Some(message)))
+  }
 
   override lazy val domain: String = config.getString("pandomain.domain").get
   override lazy val awsSecretAccessKey: String = config.getString("pandomain.aws.secret").get
