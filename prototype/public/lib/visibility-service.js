20e18880f8dcd329979a9a8ec09ecde7e62b43ca
Chris Finch <chris.finch@guardian.co.uk>
2014-09-18 11:28:07 +0100

    Intellij reformat of all scss and js in /prototype

--- prototype/public/lib/visibility-service.js
+++ prototype/public/lib/visibility-service.js
@@ -9,56 +9,56 @@
 import angular from 'angular';
 
 angular.module('wfVisibilityService', [])
-  .factory('wfVisibilityService', [function() {
+    .factory('wfVisibilityService', [function () {
 
-    function getHiddenProp() {
-      var prefixes = ['webkit','moz','ms','o'];
+        function getHiddenProp() {
+            var prefixes = ['webkit', 'moz', 'ms', 'o'];
 
-      // if 'hidden' is natively supported just return it
-      if ('hidden' in document) return 'hidden';
+            // if 'hidden' is natively supported just return it
+            if ('hidden' in document) return 'hidden';
 
-      // otherwise loop over all the known prefixes until we find one
-      for (var i = 0; i < prefixes.length; i++){
-          if ((prefixes[i] + 'Hidden') in document)
-              return prefixes[i] + 'Hidden';
-      }
+            // otherwise loop over all the known prefixes until we find one
+            for (var i = 0; i < prefixes.length; i++) {
+                if ((prefixes[i] + 'Hidden') in document)
+                    return prefixes[i] + 'Hidden';
+            }
 
-      // otherwise it's not supported
-      return null;
-    }
-
-
-    function addVisibilityChangeListener(listener) {
-      var visProp = getHiddenProp(),
-          evtname = visProp.replace(/[H|h]idden/,'') + 'visibilitychange';
-
-      document.addEventListener(evtname, listener);
-    }
+            // otherwise it's not supported
+            return null;
+        }
 
 
-    class VisibilityService {
+        function addVisibilityChangeListener(listener) {
+            var visProp = getHiddenProp(),
+                evtname = visProp.replace(/[H|h]idden/, '') + 'visibilitychange';
 
-      onVisibilityChanged(callback) {
-        addVisibilityChangeListener((function() {
-          callback({ visibility: !this.isHidden() });
-        }).bind(this));
-      }
+            document.addEventListener(evtname, listener);
+        }
 
-      isHidden() {
-        var prop = getHiddenProp();
-        if (!prop) return false;
 
-        return document[prop];
-      }
+        class VisibilityService {
 
-    }
+            onVisibilityChanged(callback) {
+                addVisibilityChangeListener((function () {
+                    callback({ visibility: !this.isHidden() });
+                }).bind(this));
+            }
 
-    return new VisibilityService();
+            isHidden() {
+                var prop = getHiddenProp();
+                if (!prop) return false;
 
-  }])
+                return document[prop];
+            }
 
-  .run(['$rootScope', 'wfVisibilityService', function($rootScope, wfVisibilityService) {
-    wfVisibilityService.onVisibilityChanged(function(data) {
-      $rootScope.$broadcast('visibility.changed', data);
-    });
-  }]);
+        }
+
+        return new VisibilityService();
+
+    }])
+
+    .run(['$rootScope', 'wfVisibilityService', function ($rootScope, wfVisibilityService) {
+        wfVisibilityService.onVisibilityChanged(function (data) {
+            $rootScope.$broadcast('visibility.changed', data);
+        });
+    }]);
