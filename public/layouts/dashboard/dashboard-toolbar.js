66a75595e09065aa23037278528a43f24f78d05a
Chris Finch <chris.finch@guardian.co.uk>
2014-09-18 11:28:07 +0100

    Intellij reformat of all scss and js in /prototype

--- public/layouts/dashboard/dashboard-toolbar.js
+++ public/layouts/dashboard/dashboard-toolbar.js
@@ -1,87 +1,86 @@
-
 import angular from 'angular';
 
 import 'lib/date-service';
 import 'lib/filters-service';
 
 angular.module('wfDashboardToolbar', ['wfFiltersService', 'wfDateService'])
-  .controller('wfDashboardToolbarController', ['$scope', 'wfFiltersService', 'wfDateParser', 'prodOfficeService', 'sections', function($scope, wfFiltersService, wfDateParser, prodOfficeService, sections) {
+    .controller('wfDashboardToolbarController', ['$scope', 'wfFiltersService', 'wfDateParser', 'prodOfficeService', 'sections', function ($scope, wfFiltersService, wfDateParser, prodOfficeService, sections) {
 
-    $scope.selectedProdOffice = wfFiltersService.get('prodOffice');
+        $scope.selectedProdOffice = wfFiltersService.get('prodOffice');
 
-    $scope.prodOffices = prodOfficeService.getProdOffices();
-
-
-    $scope.selectedSection = wfFiltersService.get('section');
-    $scope.sections = sections;
-
-    $scope.$watch('selectedProdOffice', function(){
-      $scope.$emit('filtersChanged.prodOffice', $scope.selectedProdOffice);
-    });
-
-    $scope.$watch('selectedSection', function(){
-      $scope.$emit('filtersChanged.section', $scope.selectedSection);
-    });
+        $scope.prodOffices = prodOfficeService.getProdOffices();
 
 
-    $scope.dateOptions = wfDateParser.getDaysThisWeek();
-    var selectedDate = wfFiltersService.get('selectedDate');
+        $scope.selectedSection = wfFiltersService.get('section');
+        $scope.sections = sections;
 
-    // ensure that the date from the URL is the same object as the
-    // one used in the Select drop-down, as its compared with ===
-    $scope.dateOptions.forEach(function(date) {
-      if (date.isSame(selectedDate)) {
-        selectedDate = date;
-      }
-    });
+        $scope.$watch('selectedProdOffice', function () {
+            $scope.$emit('filtersChanged.prodOffice', $scope.selectedProdOffice);
+        });
 
-    $scope.selectedDate = selectedDate;
+        $scope.$watch('selectedSection', function () {
+            $scope.$emit('filtersChanged.section', $scope.selectedSection);
+        });
 
-    $scope.deadlineSelectActive = function() {
-      return $scope.selectedDate && typeof($scope.selectedDate) != 'string';
-    };
 
-    $scope.$watch('selectedDate', function() {
-      $scope.$emit('filtersChanged.selectedDate', $scope.selectedDate);
-    });
+        $scope.dateOptions = wfDateParser.getDaysThisWeek();
+        var selectedDate = wfFiltersService.get('selectedDate');
 
-  }])
+        // ensure that the date from the URL is the same object as the
+        // one used in the Select drop-down, as its compared with ===
+        $scope.dateOptions.forEach(function (date) {
+            if (date.isSame(selectedDate)) {
+                selectedDate = date;
+            }
+        });
 
-  .directive('wfToolbarOption', [function() {
-    return {
-      restrict: 'A',
-      require: '^ngModel',
-      scope: {
-        ngModel: '=',
-        optionName: '@wfToolbarOption',
-        value: '@wfToolbarOptionValue'
-      },
+        $scope.selectedDate = selectedDate;
 
-      link: function(scope, elem, attrs, ngModel) {
-
-        var className = 'dashboard-toolbar__' + scope.optionName + '-option',
-            activeClass = className + '--active',
-
-            value = scope.value || undefined;
-
-        ngModel.$render = function() {
-          var newValue = ngModel.$modelValue;
-
-          if (newValue == value) {
-            elem.addClass(activeClass);
-            elem.removeClass(className);
-          } else {
-            elem.addClass(className);
-            elem.removeClass(activeClass);
-          }
+        $scope.deadlineSelectActive = function () {
+            return $scope.selectedDate && typeof($scope.selectedDate) != 'string';
         };
 
-        elem.on('click', function(event) {
-          ngModel.$setViewValue(value);
-
-          ngModel.$render();
-          scope.$apply();
+        $scope.$watch('selectedDate', function () {
+            $scope.$emit('filtersChanged.selectedDate', $scope.selectedDate);
         });
-      }
-    };
-  }]);
+
+    }])
+
+    .directive('wfToolbarOption', [function () {
+        return {
+            restrict: 'A',
+            require: '^ngModel',
+            scope: {
+                ngModel: '=',
+                optionName: '@wfToolbarOption',
+                value: '@wfToolbarOptionValue'
+            },
+
+            link: function (scope, elem, attrs, ngModel) {
+
+                var className = 'dashboard-toolbar__' + scope.optionName + '-option',
+                    activeClass = className + '--active',
+
+                    value = scope.value || undefined;
+
+                ngModel.$render = function () {
+                    var newValue = ngModel.$modelValue;
+
+                    if (newValue == value) {
+                        elem.addClass(activeClass);
+                        elem.removeClass(className);
+                    } else {
+                        elem.addClass(className);
+                        elem.removeClass(activeClass);
+                    }
+                };
+
+                elem.on('click', function (event) {
+                    ngModel.$setViewValue(value);
+
+                    ngModel.$render();
+                    scope.$apply();
+                });
+            }
+        };
+    }]);
--- public/lib/settings-service.js
+++ public/lib/settings-service.js
@@ -7,45 +7,45 @@ import angular from 'angular';
 import './local-storage-adapter';
 
 angular.module('wfSettingsService', ['wfLocalStorageAdapter'])
-  .factory('wfSettingsService', ['$log', 'wfLocalStorageAdapter', function($log, localStorageAdapter) {
+    .factory('wfSettingsService', ['$log', 'wfLocalStorageAdapter', function ($log, localStorageAdapter) {
 
-    class Settings {
+        class Settings {
 
-      constructor() {
-        try {
-          this._settings = localStorageAdapter.getObject('settings') || {};
+            constructor() {
+                try {
+                    this._settings = localStorageAdapter.getObject('settings') || {};
 
-        } catch (err) {
-          $log.error('Could not retrieve settings from localStorage: ' + err);
-          this._settings = {};
+                } catch (err) {
+                    $log.error('Could not retrieve settings from localStorage: ' + err);
+                    this._settings = {};
+                }
+
+                // TODO: add listeners to local storage events
+            }
+
+            get(key) {
+                return this._settings[key];
+            }
+
+            /**
+             * Set a setting. Persists in browser localStorage.
+             */
+                set(key, value) {
+                this._settings[key] = value;
+
+                try {
+                    localStorageAdapter.set('settings', this._settings);
+
+                } catch (err) {
+                    // Silently fail on local storage error
+                    $log.error('Error setting value in localStorage: ' + err);
+                }
+
+                return this;
+            }
+
         }
 
-        // TODO: add listeners to local storage events
-      }
+        return new Settings();
 
-      get(key) {
-        return this._settings[key];
-      }
-
-      /**
-       * Set a setting. Persists in browser localStorage.
-       */
-      set(key, value) {
-        this._settings[key] = value;
-
-        try {
-          localStorageAdapter.set('settings', this._settings);
-
-        } catch (err) {
-          // Silently fail on local storage error
-          $log.error('Error setting value in localStorage: ' + err);
-        }
-
-        return this;
-      }
-
-    }
-
-    return new Settings();
-
-  }]);
+    }]);
