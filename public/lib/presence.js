cf36cbf92064f3e60a1dca09614347349ecab1a8
Paul Roberts <paul.roberts@guardian.co.uk>
2014-09-19 14:14:52 +0100

    some refactoring; send an event when a subscribed request is received

--- public/lib/presence.js
+++ public/lib/presence.js
@@ -4,39 +4,41 @@ define([], function () {
 
     module.factory('wfPresenceService', ['$rootScope', function($rootScope) {
 
+        var $scope = $rootScope.$new();
+
         var self = {};
 
         //self.endpoint = "ws://presence-Presence-OWDNPQCCLV33-1270668035.eu-west-1.elb.amazonaws.com/socket";
         self.endpoint = "ws://localhost:9000/socket";
 
+        var messageHandlers = {
+            "connectionTest": function() {
+                /* XXX TODO : how should I reply to this connectionTest request? */
+                // console.log("recieved connection test request from server")
+            },
+            "subscribed": function(data) {
+                self.clientId = data.clientId;
+                /* XXX TODO : share the initial state, which will have been sent to us here */
+                $rootScope.$broadcast("presence.subscribed", data);
+            },
+            "updated": function(data) {
+                console.log("XXX PMR receiving updated message");
+                $rootScope.$broadcast("presence.status", data);
+            }
+        }
+
+        function messageHandler(msgJson) {
+            var msg = JSON.parse(msgJson);
+            console.log("PMR: messageHandler", msgJson);
+            if(typeof messageHandlers[msg.action] === "function") {
+                messageHandlers[msg.action](msg.data);
+            } else {
+                console.log("receive unknown message action: " + msg.action);
+                $rootScope.$broadcast("presence.error.unknownAction", msg);
+            }
+        }
+
         var _socket = new Promise( function(resolve, reject) {
-
-            var messageHandlers = {
-                "connectionTest": function() {
-                    /* XXX TODO : how should I reply to this connectionTest request? */
-                    // console.log("recieved connection test request from server")
-                },
-                "subscribed": function(data) {
-                    self.clientId = data.clientId;
-                    /* XXX TODO : share the initial state, which will have been sent to us here */
-                },
-                "updated": function(data) {
-                    console.log("XXX PMR receiving updated message");
-                    $rootScope.$broadcast("presence.status", data);
-                }
-            }
-
-            function messageHandler(msgJson) {
-                var msg = JSON.parse(msgJson);
-                console.log("PMR: messageHandler", msgJson);
-                if(typeof messageHandlers[msg.action] === "function") {
-                    messageHandlers[msg.action](msg.data);
-                } else {
-                    console.log("receive unknown message action: " + msg.action);
-                    $rootScope.$broadcast("presence.error.unknownAction", msg);
-                }
-            }
-
             var s = new WebSocket(self.endpoint);
             s.onerror   = reject;
             s.onopen    = function () { resolve(s); }
@@ -80,13 +82,11 @@ define([], function () {
             });
         }
 
-        /* provide an optional callback to execute when the message
-         * arrives. Either way an event will be broadcast. */
-        self.articleSubscribe = function(articleIds, cb) {
-            console.log("subscribing to:", articleIds);
+        self.articleSubscribe = function(articleIds) {
             var ids = (Array.isArray(articleIds)) ? articleIds : Array(articleIds);
             var p = sendJson(makeSubscriptionRequest(ids));
-            if(typeof cb == "function") { p.then(cb); }
+            p.then(function () { console.log("sent request: ", makeSubscriptionRequest(ids)) });
+            return p;
         }
 
         return self;
@@ -101,7 +101,17 @@ define([], function () {
                 return _.pluck(_.flatten(_.values(content)), "composerId")
             }
 
-            function doSubscription() {
+            function doSubscription(ids) {
+                var initialData = new Promise(function (resolve, reject) {
+
+                    console.log("sending sub request");
+
+                    wfPresenceService.articleSubscribe(ids);
+                    $scope.$on("presence.subscribed", function (ev, data) {
+                        console.log("presence.subscribed", data);
+                        resolve();
+                    });
+                });
             }
 
             $scope.$watch(function($scope) {
