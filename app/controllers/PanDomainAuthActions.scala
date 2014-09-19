2675a803bed79c714cac8dad5c1f4362f35c7684
Stephen Wells <stephen.wells@guardian.co.uk>
2014-09-19 11:52:14 +0100

    better messaging for invalid users

--- app/controllers/PanDomainAuthActions.scala
+++ app/controllers/PanDomainAuthActions.scala
@@ -21,6 +21,16 @@ trait PanDomainAuthActions extends AuthActions with Results {
     Ok(views.html.login(Some(message)))
   }
 
+
+  override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
+    if( (claimedAuth.user.email endsWith ("@guardian.co.uk)")) && !claimedAuth.multiFactor) {
+      s"${claimedAuth.user.email} is not valid for use with Workflow as you need to have two factor authentication enabled." +
+       s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools"
+    } else {
+      s"${claimedAuth.user.email} is not valid for use with Workflow. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in"
+    }
+  }
+
   override lazy val domain: String = config.getString("pandomain.domain").get
   override lazy val awsSecretAccessKey: String = config.getString("pandomain.aws.secret").get
   override lazy val awsKeyId: String = config.getString("pandomain.aws.keyId").get
