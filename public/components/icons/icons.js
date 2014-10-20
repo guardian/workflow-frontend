88fdb91f346ce60b79d0cbbe7af7d33e8296483a
Chris Finch <chris.finch@guardian.co.uk>
2014-10-20 15:35:12 +0100

    declare icon url as a function

--- public/components/icons/icons.js
+++ public/components/icons/icons.js
@@ -11,7 +11,7 @@ function wfIconDirective() {
 
     return {
         restrict: 'A',
-        template: '<img ng-src="{{ iconUrl }}" class="wf-icon"/>',
+        template: '<img ng-src="{{ iconUrl() }}" class="wf-icon"/>',
         scope: {
             wfIcon: '@',
             wfIconActive: '=' // boolean
@@ -19,22 +19,7 @@ function wfIconDirective() {
         link: function($scope) {
             var urlPostfix = $scope.wfIconActive !== false ? 'active' : 'inactive';
 
-            function getIconUrl () {
-              return ICON_FILE + '#' + $scope.wfIcon + '-' + urlPostfix;
-            }
-
-            $scope.iconUrl = getIconUrl();
-
-            var currentIcon = $scope.wfIcon;
-
-            $scope.$watch('wfIcon', function () {
-                if (currentIcon !== $scope.wfIcon) { // TODO: Better way of stopping all icon directives being notified
-                    currentIcon = $scope.wfIcon;
-                    $scope.iconUrl = getIconUrl();
-                }
-            });
-
-
+            $scope.iconUrl = () => ICON_FILE + '#' + $scope.wfIcon + '-' + urlPostfix;
         }
     };
 
