1f5512adece7749aea7e41857550ed77ddf05ecb
Robert Kenny <robert.kenny@guardian.co.uk>
2014-11-06 17:03:17 +0000

    User Angular  instead of window

--- public/layouts/dashboard/dashboard-user.js
+++ public/layouts/dashboard/dashboard-user.js
@@ -4,9 +4,9 @@ import 'lib/user';
 
 angular
     .module('wfDashboardUser', ['wfUser'])
-    .controller('wfDashboardUserController', ['$scope', 'wfUser', function ($scope, user) {
+    .controller('wfDashboardUserController', ['$scope', '$window', 'wfUser', function ($scope, $window, user) {
         $scope.displayName = user.displayName; 
         $scope.logout = function(dialogText) {
-            if(window.confirm(dialogText)) { window.location.replace('/logout'); }
+            if($window.confirm(dialogText)) { $window.location.replace('/logout'); }
         }
     }])
