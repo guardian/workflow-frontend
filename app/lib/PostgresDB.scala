1fc4d0070fb358db93d4d41498baf0fe8e9e08b0
Chris Finch <chris.finch@guardian.co.uk>
2014-11-06 12:38:29 +0000

    WIP

--- app/lib/PostgresDB.scala
+++ app/lib/PostgresDB.scala
@@ -62,9 +62,10 @@ object PostgresDB {
            .list.map {
             case (stubData, contentData) =>
           val stub    = Stub.fromStubRow(stubData)
-          val content = WorkflowContent.fromContentRow(contentData).copy(
-            section = Some(Section(stub.section))
-          )
+          val content = WorkflowContent.fromContentRow(contentData)
+//          val content = WorkflowContent.fromContentRow(contentData).copy(
+//            section = Some(Section(stub.section, true, 99))
+//          )
           DashboardRow(stub, content)
       }
 
