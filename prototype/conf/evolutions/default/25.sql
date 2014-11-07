023e1d312408ba2e3ea5758e78ebbe2d5ff332d3
Chris Finch <chris.finch@guardian.co.uk>
2014-11-07 16:56:37 +0000

    Added foriegn key constraint and separated evolutions

--- prototype/conf/evolutions/default/25.sql
+++ prototype/conf/evolutions/default/25.sql
@@ -1,9 +1,7 @@
 # --- !Ups
 
 create table desk(pk serial primary key, desk varchar(128) not null unique);
-create table section_desk_mapping(section_id integer not null,desk_id integer not null);
 
 # --- !Downs
 
 drop table desk;
-drop table section_desk_mapping;
