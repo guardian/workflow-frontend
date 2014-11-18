d0db31abe3b7d4b0dce8f2aa98a2203d9adf9d2e
james@jamespamplin.com <james@jamespamplin.com>
2014-11-18 17:07:01 +0000

    New icon approach. Icon refs loaded via System JS injected into head. Reused by svg use element

--- public/components/icons/icons.js
+++ public/components/icons/icons.js
@@ -1,37 +1,67 @@
 
 import angular from 'angular';
 
-// Path to aggregated stacked icons
-var ICON_FILE = '/assets/components/icons/icons.svg';
+// Path to aggregated icons
+var ICON_FILE = 'components/icons/icons.svg';
+
+// SVG Namespace
+var SVG_NS = 'http://www.w3.org/2000/svg';
+var XLINK_NS = 'http://www.w3.org/1999/xlink';
+
 
 angular.module('wfIcons', [])
+    .run(wfIconInit)
     .directive('wfIcon', wfIconDirective);
+
+
+// Async loads the icon file
+function wfIconInit() {
+    System.import(ICON_FILE + '!text').then((iconSprite) => {
+        angular.element(document.head).append(iconSprite);
+
+    }, (err) => { window.onerror(err); });
+}
+
 
 function wfIconDirective() {
 
     return {
         restrict: 'A',
-        template: '<img ng-src="{{ iconUrl() }}" class="wf-icon"/>',
         scope: {
             wfIcon: '@',
             wfIconActive: '=', // boolean
             wfIconDark: '=' //boolean
         },
-        link: function($scope) {
-            $scope.iconUrl = () => { 
-                var postFixOptions = [
-                    $scope.wfIconActive !== false ? 'active' : 'inactive',
-                    $scope.wfIconDark === true ? 'dark-bg' : ''
-                ];
+        link: function($scope, $element) {
 
-                var urlPostfix = postFixOptions.map(function(postFixOption) {
-                    return postFixOption == '' ? '' : '-' + postFixOption; 
-                }).reduce(function(previous, current, i, a) {
-                    return previous + current;
-                });
-                
-                return ICON_FILE + '#' + $scope.wfIcon + urlPostfix;
-            }
+            var iconElem = document.createElementNS(SVG_NS, 'svg');
+            // iconElem.setAttribute('class', 'wf-icon');
+            iconElem.setAttribute('viewBox', '0 0 128 128');
+
+            var useElem = iconElem.appendChild(document.createElementNS(SVG_NS, 'use'));
+            useElem.setAttributeNS(XLINK_NS, 'href', '#icon-' + $scope.wfIcon);
+
+
+            $element.append(iconElem);
+
+            $scope.$watch('wfIconActive', (newValue, oldValue) => {
+                iconElem.setAttribute('class', 'wf-icon--' + (newValue !== false ? 'active' : 'inactive'));
+            });
+
+            // $scope.iconUrl = () => {
+            //     var postFixOptions = [
+            //         $scope.wfIconActive !== false ? 'active' : 'inactive',
+            //         $scope.wfIconDark === true ? 'dark-bg' : ''
+            //     ];
+
+            //     var urlPostfix = postFixOptions.map(function(postFixOption) {
+            //         return postFixOption == '' ? '' : '-' + postFixOption;
+            //     }).reduce(function(previous, current, i, a) {
+            //         return previous + current;
+            //     });
+
+            //     return ICON_DIR + ICON_FILE + '#' + $scope.wfIcon + urlPostfix;
+            // };
         }
     };
 
