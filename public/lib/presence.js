04e65f99d028a997fc6e25f7656df2aa490bb985
Chris Finch <chris.finch@guardian.co.uk>
2014-11-19 16:02:11 +0000

    add presence indicators to drawer

--- public/lib/presence.js
+++ public/lib/presence.js
@@ -1,4 +1,5 @@
 import _ from "underscore"
+import { wfPresenceIndicatorsDirective } from 'components/presence-indicator/presence-indicators';
 var module = angular.module('wfPresenceService', []);
 
 module.factory('wfPresenceService', ['$rootScope', '$log', 'config', 'wfFeatureSwitches', 'wfUser', function($rootScope, $log, config, wfFeatureSwitches, wfUser) {
@@ -165,46 +166,4 @@ module.directive(
     }]);
 
 
-module.directive('wfPresenceIndicators', ['$rootScope', 'wfPresenceService', function($rootScope, wfPresenceService) {
-
-    return {
-        restrict: 'E',
-        templateUrl: "/assets/components/presence-indicator/presence-indicators.html",
-        scope: {
-            id: "=presenceId"
-        },
-        link: function($scope) {
-
-            function applyCurrentState(currentState) {
-                if(currentState.length === 0) {
-                    $scope.presences = [{ status: "free", indicatorText: ""}];
-                } else {
-                    $scope.presences = _.map(
-                        _.uniq(currentState, false, function(s) { return s.clientId.person.email; }),
-                        function (pr) {
-                            var person = pr.clientId.person;
-                            return { indicatorText:
-                                     (person.firstName.charAt(0) + person.lastName.charAt(0)).toUpperCase(),
-                                     longText: [person.firstName, person.lastName].join(" "),
-                                     email: person.email,
-                                     status: "present" };
-                        });
-                }
-            }
-
-            if ($scope.id) {
-                wfPresenceService.initialData($scope.id).then(function (currentState) {
-                    applyCurrentState(currentState);
-                }, function (err) {
-                    $log.error("Error getting initial data:", err);
-                });
-
-                $scope.$on("presence.status", function(ev, data) {
-                    if($scope.id === data.subscriptionId) {
-                        applyCurrentState(data.currentState);
-                    }
-                });
-            }
-        }
-    };
-}]);
+module.directive('wfPresenceIndicators', ['$rootScope', 'wfPresenceService', wfPresenceIndicatorsDirective]);
