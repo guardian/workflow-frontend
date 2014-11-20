f0c329ca53d41b7cb24890791a5101d94a0e170c
Robert Kenny <kenoir@gmail.com>
2014-11-20 09:31:55 +0000

    fix tests
    Conflicts:
    	prole/test/resources/flex-notification1.json
    	prole/test/resources/flex-notification2.json

--- prole/test/WireStatus.scala
+++ prole/test/WireStatus.scala
@@ -9,28 +9,28 @@ class WireStatusModelSpec extends FunSuite with ShouldMatchers with ResourcesHel
   test("parse flex notification 1") {
     val resource = slurp("flex-notification1.json").getOrElse(throw new RuntimeException("could not find test resource"))
     val JsSuccess(ws, _) = Json.parse(resource).validate[WireStatus]
-    ws.composerId should equal("53b3e0863004701d9b381539")
+    ws.composerId should equal("546c965bb0c64447ac22fa62")
     ws.path should equal (None)
     ws.`type` should equal ("article")
     ws.whatChanged should equal ("setting")
-    ws.user should equal (Some("Stephen Wells"))
-    ws.lastModified should equal (new DateTime(2014, 7, 2, 14, 18, 58, 65))
+    ws.user should equal (Some("Robert Kenny"))
+    ws.lastModified should equal (new DateTime("2014-11-19T15:05:18.002Z"))
     ws.tagSections should equal (Nil)
     ws.commentable should equal (false)
     ws.published should equal (false)
     ws.lastMajorRevisionDate should equal (None)
-    ws.revision should equal (2)
+    ws.revision should equal (4)
   }
 
   test("parse flex notification 2") {
     val resource = slurp("flex-notification2.json").getOrElse(throw new RuntimeException("could not find test resource"))
     val JsSuccess(ws, _) = Json.parse(resource).validate[WireStatus]
-    ws.composerId should equal("53b3e0863004701d9b381539")
+    ws.composerId should equal("546c965bb0c64447ac22fa62")
     ws.path should equal (None)
     ws.`type` should equal ("article")
     ws.whatChanged should equal ("published")
     ws.user should equal (None)
-    ws.lastModified should equal (new DateTime(2014, 7, 2, 14, 18, 58, 687))
+    ws.lastModified should equal (new DateTime("2014-11-19T15:05:18.002Z"))
     ws.tagSections should equal (Nil)
     ws.commentable should equal (false)
     ws.published should equal (true)
