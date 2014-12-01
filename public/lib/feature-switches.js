39643e039de320ac54d4f5ce820437a2a5aedf36
Paul Roberts <paul.roberts@guardian.co.uk>
2014-12-01 11:12:11 +0000

    put incopy export behind a (cookie) feature switch

--- public/lib/feature-switches.js
+++ public/lib/feature-switches.js
@@ -8,13 +8,24 @@ define([], function () {
         // dummy switches - eventually this will come from the server
         // via an API call
 
-        var staticSwitchData = { "presence-indicator": document.cookie.search("presence-indicator=1(;|$)") != -1 }
+        function simpleCookie(cookieName) {
+            return document.cookie.search(cookieName + "=1(;|$)") != -1;
+        }
+
+        var staticSwitchData = {
+            "presence-indicator": simpleCookie("presence-indicator"),
+            "incopy-export": simpleCookie("incopy-export")
+        }
         var switches = new Promise(function(resolve, reject) {
             resolve(staticSwitchData);
         });
 
         self.getSwitch = function(sw) {
             return switches.then(function(switches) { return switches[sw]; })
+        };
+
+        self.withSwitch = function(sw, f) {
+            self.getSwitch(sw).then(f);
         };
 
         return self;
