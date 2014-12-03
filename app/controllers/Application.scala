c7df4be817fa64d571f2aff1a51d40bd0d407031
Paul Roberts <paul.roberts@guardian.co.uk>
2014-12-03 12:25:04 +0000

    move feature cookies bits into a separate object

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -44,33 +44,4 @@ object Application extends Controller with PanDomainAuthActions {
       Ok(views.html.app(title, Some(user), config))
     }
   }
-
-  def featureList(implicit request: Request[_]): Map[String, Boolean] = {
-    def featureDef(name: String): (String, Boolean) =
-      (name, request.cookies.get(name).map(_.value == "1").getOrElse(false))
-    Map(featureDef("incopy-export"))
-  }
-
-  def makeCookie[A](name: String, value: Boolean => Boolean)
-                (implicit request: Request[A]): Option[Cookie] =
-    featureList.get(name).map { curVal =>
-      Cookie(name, if(value(curVal)) "1" else "0", Some(Int.MaxValue),
-             httpOnly = false)
-    }
-
-  def featureSwitch(name: String, value: Boolean => Boolean) =
-    AuthAction { implicit request =>
-      makeCookie(name, value).map(cookie =>
-        TemporaryRedirect("/").withCookies(cookie))
-        .getOrElse(BadRequest(s"Unknown cookie $name"))
-    }
-
-  def featureSwitchOn(name: String) =
-    featureSwitch(name, _ => true)
-
-  def featureSwitchOff(name: String) =
-    featureSwitch(name, _ => false)
-
-  def featureSwitchToggle(name: String) =
-    featureSwitch(name, !_)
 }
