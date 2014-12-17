21b174a94f4c82227cd675b167b00d68ac118461
Paul Roberts <paul.roberts@guardian.co.uk>
2014-12-17 17:35:54 +0000

    improve the shared secret code so that it expects an HMAC sig of the current time (modulo 60 seconds or so)

--- app/controllers/SharedSecretAuth.scala
+++ app/controllers/SharedSecretAuth.scala
@@ -14,18 +14,19 @@ object SharedSecretAuthAction extends ActionBuilder[Request] {
   val sharedKey = Config.getConfigString("api.sharedsecret")
     .right.getOrElse("changeme").getBytes
 
+  val mask = Math.pow(2, 16).toLong - 1
+
   // openssl sha1 -hmac "ABC"
-  def sharedSecret = {
-    if(devmode) {
-      "changeme"
-    } else {
-      val plaintext = ((new Date().getTime) & ~(Math.pow(2,16).toLong - 1)).toString
-      sign(plaintext, sharedKey)
-    }
+  def sharedSecret: Seq[String] = {
+    val code = ((new Date().getTime) & ~(mask))
+    // give some grace on either side
+    List(code - (mask + 1), code, code + (mask + 1)).map(time => sign(time.toString, sharedKey))
   }
 
+  def matchesSharedSecret(candidate: String): Boolean = sharedSecret.exists({ x => println(s"comparing $x to $candidate"); x == candidate })
+
   def isInOnTheSecret(req: Request[_]): Boolean =
-    req.cookies.get(cookieName).map(_.value == sharedSecret).getOrElse(false)
+    req.cookies.get(cookieName).map(cookie => matchesSharedSecret(cookie.value)).getOrElse(false)
 
   def invokeBlock[A](req: Request[A], block: (Request[A]) => Future[Result]) =
     if(!isInOnTheSecret(req))
