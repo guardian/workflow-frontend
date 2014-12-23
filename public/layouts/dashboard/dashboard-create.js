b39e70b8f2e913212d82bc7b69fea40f75f69448
Chris Finch <chris.finch@guardian.co.uk>
2014-12-23 14:14:55 +0000

    remove console.log - agian...

--- public/layouts/dashboard/dashboard-create.js
+++ public/layouts/dashboard/dashboard-create.js
@@ -5,20 +5,19 @@ import 'lib/content-service';
 angular
     .module('wfDashboardCreate', ['wfContentService'])
     .controller('wfDashboardCreateController', ['$scope', 'wfContentService', function ($scope, contentService) {
-        $scope.options = contentService.getTypes(); 
+        $scope.options = contentService.getTypes();
 
         $scope.createContent = function(contentType) {
             $scope.$emit('stub:create', contentType);
-        }
+        };
         $scope.importContent = function() {
             $scope.$emit('content:import');
-        }
+        };
     }])
     .directive('wfDropdownToggle', ['$document', function($document){
         return {
             restrict: 'A',
             link: function(scope, element) {
-console.log("foo");
                 scope.toggleDropdown = function() { scope.showDropdown = !scope.showDropdown }
                 element.bind('click', function(event) { event.stopPropagation(); });
                 $document.bind('click', function(){ scope.showDropdown = false; scope.$apply();});
