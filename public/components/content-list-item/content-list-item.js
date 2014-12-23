09c982cf894184ddc506a7f79e837a62e2b8becd
Chris Finch <chris.finch@guardian.co.uk>
2014-12-23 11:35:00 +0000

    Fix for non-functioning filters

--- public/components/content-list-item/content-list-item.js
+++ public/components/content-list-item/content-list-item.js
@@ -145,10 +145,7 @@ function wfContentItemParser(config, statuses, wfLocaliseDateTimeFilter, wfForma
 var wfContentListItem = function ($rootScope, $q, $compile, $http, $templateCache, wfColumnService) {
     return {
         restrict: 'A',
-//        replace: true,
-//        template: () => {
-//            return $templateRequest.get('/assets/components/content-list-item/content-list-item.html');
-//        },
+        templateUrl: '/assets/components/content-list-item/content-list-item-container.html',
         scope: {
             contentItem: '=',
             contentList: '=',
@@ -190,8 +187,7 @@ var wfContentListItem = function ($rootScope, $q, $compile, $http, $templateCach
 
                 $q.all(promises).then(() => {
                     var compiled = $compile(columns.join(''))($scope);
-                    elem.replaceWith(compiled);
-                    elem = compiled;
+                    elem.empty().append(compiled);
                 });
             }
 
