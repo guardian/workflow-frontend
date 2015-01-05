dc1aa82b41d157caa668c8686ee3492b7a25cc51
Robert Kenny <kenoir@gmail.com>
2015-01-05 16:53:12 +0000

    adds whitelist auth filter

---
+++ prototype/app/controllers/WhiteListAuth.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import play.api.mvc._
+import com.gu.workflow.lib.Config
+import com.gu.pandomainauth.action.UserRequest
+
+object WhiteListAuthFilter extends ActionFilter[UserRequest] {
+
+  override def filter[A](request:UserRequest[A]) = Future.successful {
+    val user = request.user
+    val whitelist = Config.getConfigStringList("application.admin.whitelist").right.getOrElse(List())
+
+    if(whitelist.contains(user.email)) {
+      None
+    } else {
+      Some(Results.Forbidden(views.html.unauthorisedUser(user.toJson))) 
+    }
+  }
+
+}
