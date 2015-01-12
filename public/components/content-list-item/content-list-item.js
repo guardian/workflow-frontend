8f526592ac2f255a83c5228d689b17a2343032f4
Robert Kenny <kenoir@gmail.com>
2015-01-12 14:43:16 +0000

    use contentItem decorator, only show word count for articles

--- public/components/content-list-item/content-list-item.js
+++ public/components/content-list-item/content-list-item.js
@@ -86,6 +86,7 @@ function wfContentItemParser(config, statuses, wfLocaliseDateTimeFilter, wfForma
             // TODO: Stubs have a different structure to content items
             this.id = item.id || item.stubId;
             this.composerId = item.composerId;
+            this.wordCount = item.wordCount;
 
             this.headline = item.headline;
             this.standfirst = this.truncateString(item.standfirst);
