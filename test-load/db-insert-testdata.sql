be5692cc49ed2876c0f70d4cbe9e1ddefa111ff1
james@jamespamplin.com <james@jamespamplin.com>
2015-01-21 17:10:44 +0000

    Modified to work with new purge query

--- test-load/db-insert-testdata.sql
+++ test-load/db-insert-testdata.sql
@@ -14,8 +14,8 @@ SELECT 'LOADTEST_' || x.id, 'LOADTEST: content-' || x.id, 'AU News', 'article', 
     FROM generate_series(1,100) AS x(id);
 
 -- Create Content to be filtered out (published in past)
-INSERT INTO public.content (composer_id, last_modified, last_modified_by, headline, status, content_type, time_published)
-SELECT 'LOADTEST_OLD_' || x.id, NOW(), 'LOADTEST_USER', 'LOADTEST: headline-old-' || x.id, 'Revise', 'article', '2014-08-14'
+INSERT INTO public.content (composer_id, last_modified, last_modified_by, headline, status, content_type, published, time_published)
+SELECT 'LOADTEST_OLD_' || x.id, NOW() - '1 week'::INTERVAL, 'LOADTEST_USER', 'LOADTEST: headline-old-' || x.id, 'Final', 'article', TRUE, '2014-08-14'
     FROM generate_series(1,900) AS x(id);
 
 INSERT INTO public.stub (composer_id, working_title, section, content_type)
