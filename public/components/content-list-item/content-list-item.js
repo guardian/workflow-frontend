91e9ecf6ad7e701fc1c7639a538c5d8e68e2b53a
Chris Finch <chris.finch@guardian.co.uk>
2015-01-23 15:12:28 +0000

    working implementation of updating assignee in drawer

--- public/components/content-list-item/content-list-item.js
+++ public/components/content-list-item/content-list-item.js
@@ -93,8 +93,9 @@ function wfContentItemParser(config, statuses, wfLocaliseDateTimeFilter, wfForma
             this.trailtext = stripHtml(item.trailtext);
             this.trailImageUrl = item.trailImageUrl;
 
-            this.assignee = item.assignee && toInitials(item.assignee) || '';
-            this.assigneeFull = item.assignee || 'unassigned';
+            this.assignee = item.assignee || 'unassigned';
+            this.assigneeEmail = item.assigneeEmail;
+            this.assigneeInitials = item.assignee && toInitials(item.assignee) || '';
 
             this.contentType = item.contentType;
             this.contentTypeTitle = toTitleCase(item.contentType);
