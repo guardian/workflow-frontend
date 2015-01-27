fa0ca5aa02e932f05d689f05327467accf99a417
Chris Finch <chris.finch@guardian.co.uk>
2015-01-27 10:56:34 +0000

    Alter loading method for content item templates and move compilation out of contentItem directive

--- public/lib/column-service.js
+++ public/lib/column-service.js
@@ -1,10 +1,12 @@
-import angular from 'angular';
-import moment from 'moment';
-import { columnDefaults } from 'lib/column-defaults'
+import angular              from 'angular';
+import moment               from 'moment';
+import { columnDefaults }   from 'lib/column-defaults'
+import startTemplate        from "components/content-list-item/content-list-item-start.html!text";
+import endTemplate          from "components/content-list-item/content-list-item-end.html!text";
 
 angular.module('wfColumnService', [])
-    .factory('wfColumnService', ['$rootScope', '$sce', 'wfPreferencesService',
-        function($rootScope, $sce, wfPreferencesService) {
+    .factory('wfColumnService', ['$rootScope', '$sce', '$http', '$templateCache', '$q', 'wfPreferencesService',
+        function($rootScope, $sce, $http, $templateCache, $q, wfPreferencesService) {
 
             class ColumnService {
 
@@ -13,14 +15,40 @@ angular.module('wfColumnService', [])
                     var self = this;
 
                     self.availableColums = columnDefaults;
+                    self.contentItemTemplate;
 
                     self.preferencePromise = wfPreferencesService.getPreference('columnConfiguration').then(function resolve (data) {
-                        self.columns = data;
-                        return self.columns;
-                    }, function reject () {
+
+                        if (typeof data[0] !== "string") {
+
+                            return reject();
+                        } else {
+
+                            self.columns = self.availableColums;
+
+                            data.forEach((colName) => {
+                                self.columns.some((availCol) => {
+                                    if (availCol.name == colName) {
+                                        availCol.active = true; // set active
+                                        return true;
+                                    } else {
+                                        return false;
+                                    }
+                                });
+                            });
+
+                            return self.columns;
+                        }
+
+                    }, reject);
+
+                    function reject () {
+
                         self.columns = self.availableColums;
+                        self.setColumns(self.columns);
+
                         return self.columns;
-                    });
+                    }
                 }
 
                 getAvailableColumns() {
@@ -31,9 +59,49 @@ angular.module('wfColumnService', [])
                     return this.columns ? Promise.resolve(this.columns) : this.preferencePromise;
                 }
 
+                getContentItemTemplate(refresh) {
+
+                    var self = this;
+
+                    if (!self.contentItemTemplate || refresh) {
+
+                        self.contentItemTemplate = self.getColumns().then((loadedColumns) => {
+
+                            loadedColumns = loadedColumns.filter((col) => {
+                                return col.active;
+                            }).map((col) => {
+                                return col.template;
+                            });
+
+                            loadedColumns.unshift(startTemplate);
+                            loadedColumns.push(endTemplate);
+
+                            self.contentItemTemplate = loadedColumns.join('');
+
+                            return self.contentItemTemplate;
+                        });
+                    };
+
+
+                    if (typeof self.contentItemTemplate.then === 'function') {
+
+                        return self.contentItemTemplate;
+                    } else {
+
+                        return Promise.resolve(self.contentItemTemplate);
+                    }
+                }
+
                 setColumns(columns) {
                     this.columns = columns;
-                    return wfPreferencesService.setPreference('columnConfiguration', columns);
+
+                    var activeColumns = columns.filter((col) => {
+                        return col.active;
+                    }).map((col) => {
+                        return col.name;
+                    });
+
+                    return wfPreferencesService.setPreference('columnConfiguration', activeColumns);
                 }
             }
 
