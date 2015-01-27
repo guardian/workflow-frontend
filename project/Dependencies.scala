24977b897f514b87a1346e00814355d07f237e8a
Stephen Wells <stephen.wells@guardian.co.uk>
2015-01-27 11:39:42 +0000

    updates to the latest panda library.
    this version of the panda library uses the directory api rather than gdata to fetch group membership.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   val akkaDependencies = Seq("com.typesafe.akka" %% "akka-agent" % "2.3.4")
 
-  val googleOAuthDependencies = Seq("com.gu" %% "pan-domain-auth-play" % "0.1.10")
+  val googleOAuthDependencies = Seq("com.gu" %% "pan-domain-auth-play" % "0.2.2")
 
   val testDependencies = Seq("org.scalatest" %% "scalatest" % "2.1.5" % "test")
 
