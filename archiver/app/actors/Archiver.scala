d3bd385f3cbb4773c80021518c004a374bd5e5f1
lindseydew <lindsey.dew@guardian.co.uk>
2015-01-29 16:43:16 +0000

    add archived at field

--- archiver/app/actors/Archiver.scala
+++ archiver/app/actors/Archiver.scala
@@ -1,12 +1,13 @@
 import akka.actor.Actor
 import com.gu.workflow.db.CommonDB
+import org.joda.time.DateTime
 import play.api.Logger
 
 class Archiver extends Actor {
   def receive = {
     case Archive => {
       val archivedItems = CommonDB.archiveOldContent
-      Logger.info(s"archived ${archivedItems} items")
+      Logger.info(s"archived ${archivedItems} items at ${DateTime.now().toString}")
     }
   }
 }
