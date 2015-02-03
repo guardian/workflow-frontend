28bc5914ea6e90440ffa2b64bae0b95d4e465b70
Robert Kenny <kenoir@gmail.com>
2015-02-03 11:09:53 +0000

    add logback logstash appender, and hardcode config

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,4 +21,6 @@ object Dependencies {
 
   val apiDocDependencies = Seq("com.wordnik" %% "swagger-play2" % "1.3.12")
 
+  val logbackDependencies = Seq("net.logstash.logback" %% "logstash-logback-encoder" % "4.1-SNAPSHOT")
+
 }
