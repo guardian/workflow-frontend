91207c695fde790e199768b04801d1f44010140e
lindseydew <lindsey.dew@guardian.co.uk>
2015-02-03 17:00:05 +0000

    add a transaction to update last_mod

--- conf/evolutions/default/40.sql
+++ conf/evolutions/default/40.sql
@@ -1,9 +1,15 @@
 
 # --- !Ups
-ALTER TABLE stub add column wf_last_modified TIMESTAMP DEFAULT now() NOT NULL;
+ALTER TABLE stub add column wf_last_modified TIMESTAMP;
 
-UPDATE stub SET wf_last_modified = (SELECT last_modified FROM content WHERE content.composer_id = stub.composer_id)
-WHERE stub.composer_id in (SELECT composer_id FROM content);
+UPDATE stub SET wf_last_modified = (SELECT last_modified FROM content WHERE content.composer_id = stub.composer_id);
+UPDATE stub SET wf_last_modified = now() where wf_last_modified IS NULL;
+
+ALTER TABLE stub ALTER COLUMN  wf_last_modified SET NOT NULL;
+
+ALTER TABLE stub ALTER COLUMN  wf_last_modified SET DEFAULT now();
+
 
 # --- !Downs
 alter table stub drop column wf_last_modified;
+
