fb1c81070fd1d3d24b67766fc3c34d42bc7f397f
lindseydew <lindsey.dew@guardian.co.uk>
2015-02-10 12:19:00 +0000

    add a foreign key constraint

--- conf/evolutions/default/42.sql
+++ conf/evolutions/default/42.sql
@@ -1,8 +1,7 @@
 # --- !Ups
-
 DELETE FROM content WHERE NOT EXISTS (SELECT * FROM stub WHERE stub.composer_id = content.composer_id);
 
-  ALTER TABLE content ADD CONSTRAINT stub_fkey FOREIGN KEY (composer_id) REFERENCES stub (composer_id);  
+ALTER TABLE content ADD CONSTRAINT stub_fkey FOREIGN KEY (composer_id) REFERENCES stub (composer_id);
 
 # --- !Downs
-  ALTER TABLE content DROP CONSTRAINT stub_fkey; 
+ALTER TABLE content DROP CONSTRAINT stub_fkey;
