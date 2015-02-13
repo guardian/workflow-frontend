647857d2d9061a74c1ab08c58e5941b93c697cfd
Paul Roberts <paul.roberts@guardian.co.uk>
2015-02-13 12:08:21 +0000

    queries for doing the text search on content (currently not stubs) and stubs alone (i.e. stubs which have no content)
    also a separate end point for testing it

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -85,6 +85,15 @@ object Api extends Controller with PanDomainAuthActions {
     Ok(Json.obj("content" -> content, "stubs" -> stubs))
   }
 
+  def contentSearch(patt: String) = APIAuthAction { implicit req =>
+    val content = PostgresDB.getContentTextSearch(patt).map(_.toString)
+    val stubs   = PostgresDB.getStubsTextSearch(patt).map(_.toString)
+    Ok(Json.obj("stubs" -> stubs.mkString("\n"),
+                "data"  -> content.mkString("\n")
+       )
+    )
+  }
+
   def content = APIAuthAction(getContentBlock)
 
   def getContentbyId(composerId: String) = CORSable(PrototypeConfiguration.apply.composerUrl) {
