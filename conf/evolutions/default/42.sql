23bd47b86fc0b48ffbd05db817ac95be69329dfd
Robert Kenny <kenoir@users.noreply.github.com>
2015-02-17 10:53:15 +0000

    Revert "Add foreign key constraint"

--- conf/evolutions/default/42.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-DELETE FROM content WHERE NOT EXISTS (SELECT * FROM stub WHERE stub.composer_id = content.composer_id);
-
-ALTER TABLE content ADD CONSTRAINT stub_fkey FOREIGN KEY (composer_id) REFERENCES stub (composer_id);
-
-# --- !Downs
-ALTER TABLE content DROP CONSTRAINT stub_fkey;
