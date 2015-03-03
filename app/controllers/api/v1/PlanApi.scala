6a1a070029be7000524c293626fa602f82f8188d
Robert Kenny <kenoir@gmail.com>
2015-03-03 16:34:14 +0000

    formats for planneditem

--- app/controllers/api/v1/PlanApi.scala
+++ app/controllers/api/v1/PlanApi.scala
@@ -2,6 +2,7 @@ package controllers
 
 import models._
 import play.api.mvc._
+import Response.Response
 
 object PlanApi extends Controller with PanDomainAuthActions with WorkflowApi {
   def plan() = APIAuthAction { request =>
