a9b49b660cf5880312a1a93d102c8fe896d3f5a8
Chris Finch <chris.finch@guardian.co.uk>
2015-03-03 16:33:08 +0000

    angular stuff

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,9 +17,35 @@ object Application extends Controller with PanDomainAuthActions {
     Redirect(routes.Application.dashboard)
   }
 
-  def plan = AuthAction { request =>
-    val user = request.user
-    Ok(views.html.plan("Plan view", Some(user), Json.obj()))
+  def plan = AuthAction.async { request =>
+    for {
+      statuses <- StatusDatabase.statuses
+      sections = SectionDB.sectionList.sortBy(_.name)
+      desks = DeskDB.deskList.sortBy(_.name)
+      sectionsInDesks = SectionDeskMappingDB.getSectionsInDesks
+    }
+    yield {
+      val user = request.user
+
+      val config = Json.obj(
+        "composer" -> Json.obj(
+          "create" -> newContentUrl,
+          "view" -> adminUrl,
+          "details" -> contentDetails
+        ),
+        "statuses" -> statuses,
+        "desks"    -> desks,
+        "sections" -> sections,
+        "sectionsInDesks" -> sectionsInDesks, // TODO: Combine desks & sectionsInDesks
+        "presenceUrl" -> PrototypeConfiguration.cached.presenceUrl,
+        "presenceClientLib" -> PrototypeConfiguration.cached.presenceClientLib,
+        "preferencesUrl" -> PrototypeConfiguration.cached.preferencesUrl,
+        "user" -> Json.parse(user.toJson),
+        "incopyExportUrl" -> PrototypeConfiguration.cached.incopyExportUrl
+      )
+
+      Ok(views.html.app("Plan View", Some(user), config))
+    }
   }
 
   def dashboard = app("Workflow")
