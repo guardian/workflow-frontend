9f21a6c11f984f3275b562292df8705c26fe0fc8
Paul Roberts <paul.roberts@guardian.co.uk>
2015-03-03 17:14:29 +0000

    make the datelist dynamically so we can compare dates

--- public/layouts/plan/plan.js
+++ public/layouts/plan/plan.js
@@ -1,4 +1,6 @@
 import angular from 'angular';
+import moment  from 'moment';
+import _       from 'lodash';
 
 angular.module('wfPlan', [])
     .service('wfPlanLoader', function () {
@@ -15,4 +17,9 @@ angular.module('wfPlan', [])
     .controller('wfPlanController', ['$scope', 'wfPlanLoader', function wfPlanController ($scope, planLoader) {
         // controller stuff
         $scope.plannedItems = planLoader.load();
+        function makeDateList() {
+            var now = moment();
+            return _.map(_.range(0, 10), (days) => moment().add(days, 'days'));
+        }
+        $scope.dateList = makeDateList();
     }]);
