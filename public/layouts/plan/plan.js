eb921a9158d3b6842d9e4ee2e3f622073a180e24
Paul Roberts <paul.roberts@guardian.co.uk>
2015-03-04 09:56:52 +0000

    date formatting shenanigans

--- public/layouts/plan/plan.js
+++ public/layouts/plan/plan.js
@@ -2,6 +2,18 @@ import angular from 'angular';
 import moment  from 'moment';
 import _       from 'lodash';
 
+function withLocale(locale, f) {
+    // can't find a way to create a new locale without
+    // changing the global locale also
+    var oldLocale = moment.locale();
+    moment.locale(locale);
+    console.log("-> locale", moment.locale());
+    var ret = f();
+    moment.locale(oldLocale);
+    console.log("<- locale", moment.locale());
+    return ret;
+}
+
 angular.module('wfPlan', [])
     .service('wfPlanLoader', [ 'wfHttpSessionService', function (http) {
         // LOAD from the API here
@@ -15,17 +27,27 @@ angular.module('wfPlan', [])
             load: loadPlanItems
         };
     }])
+    .filter('dateListFormatter', [function () {
+        return function(date) {
+            // date should be a moment() instance but we can make sure it is
+            return moment(date).calendar();
+        }
+    }])
     .controller('wfPlanController', ['$scope', 'wfPlanLoader', function wfPlanController ($scope, planLoader) {
-        moment.locale('wfPlan', {
-            calendar : {
-                lastDay : '[Yesterday]',
-                sameDay : '[Today]',
-                nextDay : '[Tomorrow]',
-                lastWeek : '[last] dddd',
-                nextWeek : 'dddd',
-                sameElse : 'L'
-            }
-        });
+        withLocale("", function () {
+            var calLocale = {
+                calendar : {
+                    lastDay : '[Yesterday]',
+                    sameDay : '[Today]',
+                    nextDay : '[Tomorrow]',
+                    lastWeek : '[last] dddd',
+                    nextWeek : 'dddd',
+                    sameElse : 'L'
+                }
+            };
+            moment.locale('wfPlan', calLocale);
+         });
+        
         // controller stuff
         $scope.plannedItems = []
         planLoader.load().then((items) => {
@@ -33,10 +55,17 @@ angular.module('wfPlan', [])
             $scope.apply(function () { $scope.plannedItems = items; })
         });
         function makeDateList() {
-            var now = moment().locale("wfPlan");
-            return _.map(_.range(0, 10), (days) => moment().add(days, 'days'));
+            return withLocale('wfPlan', () => {
+                var now = moment().startOf('day');
+                return _.map(_.range(0, 10), (days) => moment().add(days, 'days'));
+            });
         }
-        $scope.getItems = function (date) {
+        $scope.getItems = function (dateFrom, dateTo) {
+            // search all of the planned items, and find the ones that
+            // are within our date range
+            return _.filter($scope.plannedItems, (item) => {
+                return item.plannedDate
+            });
         }
         $scope.dateList = makeDateList();
     }]);
