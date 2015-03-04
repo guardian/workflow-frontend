396b993d405622157c6e35d959a1e9f95bc0a82f
Robert Kenny <kenoir@gmail.com>
2015-03-04 16:23:32 +0000

    generate bundle color from name

--- public/layouts/plan/plan.js
+++ public/layouts/plan/plan.js
@@ -38,6 +38,30 @@ angular.module('wfPlan', ['wfPlanService', 'wfPollingService'])
     }])
     .controller('wfPlanController', ['$scope', 'wfPlanLoader', function wfPlanController ($scope, planLoader) {
         withLocale("", function () {
+
+        $scope.genColor = (s) => {
+            function hashCode(str) { // java String#hashCode
+                var hash = 0;
+                for (var i = 0; i < str.length; i++) {
+                   hash = str.charCodeAt(i) + ((hash << 5) - hash);
+                }
+                return hash;
+            }
+
+            function intToARGB(i) {
+                var c = ((i>>24)&0xFF).toString(16) +
+                        ((i>>16)&0xFF).toString(16) +
+                        ((i>>8)&0xFF).toString(16);
+
+                console.log(c);
+
+                return("#" + c);
+            }
+
+            return { 'border-left-color': intToARGB(hashCode(s)) };
+        }
+
+
             var calLocale = {
                 calendar : {
                     lastDay : '[Yesterday]',
