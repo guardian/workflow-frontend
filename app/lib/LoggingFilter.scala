a8e90df512fd62a519334e09ad592cb762eb0ae0
Paul Roberts <paul.roberts@guardian.co.uk>
2015-03-17 14:26:09 +0000

    tweak the api for withMDCExecutionContext

--- app/lib/LoggingFilter.scala
+++ app/lib/LoggingFilter.scala
@@ -1,6 +1,7 @@
 package lib
 
 import com.gu.workflow.util.LoggingContext
+import LoggingContext.{withContext, withMDCExecutionContext}
 import play.api.Logger
 import play.api.mvc._
 import scala.concurrent.Future
@@ -14,22 +15,22 @@ object LoggingFilter extends Filter {
       .headOption.map(LoggingContext.fromHeader(_)).getOrElse(Map.empty[String, String])
 
     LoggingContext.withContext(headers) {
-      // use an new implicit execution context that will stash the
-      // current MDC, and then apply to all threads that are
-      // associated with this request
-      implicit val ec = LoggingContext.createExecutionContextWithMDC(
-        PrototypeConfiguration.defaultExecutionContext)
+      LoggingContext.withMDCExecutionContext(PrototypeConfiguration.defaultExecutionContext) { implicit ec =>
+        // use an new implicit execution context that will stash the
+        // current MDC, and then apply to all threads that are
+        // associated with this request
 
-      nextFilter(requestHeader).map { result =>
-        val endTime = System.currentTimeMillis
-        val requestTime = endTime - startTime
+        nextFilter(requestHeader).map { result =>
+          val endTime = System.currentTimeMillis
+          val requestTime = endTime - startTime
 
-        Logger.info(
-          s"(${result.header.status}) ${requestHeader.method} ${requestHeader.uri} " +
-            s"took ${requestTime}ms"
-        )
+          Logger.info(
+            s"(${result.header.status}) ${requestHeader.method} ${requestHeader.uri} " +
+              s"took ${requestTime}ms"
+          )
 
-        result
+          result
+        }
       }
     }
   }
