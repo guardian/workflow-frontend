b81f25aab19d8ea4de87f28cb83828308dce96ac
Philip McMahon <philip.mcmahon@guardian.co.uk>
2015-04-28 11:18:36 +0100

    Replace upsert method with insert, new PATCH endpoint to update planned items.

--- app/controllers/api/v1/PlanApi.scala
+++ app/controllers/api/v1/PlanApi.scala
@@ -7,10 +7,11 @@ import lib.Response.Response
 import models._
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.json.{JsResult, Reads, JsValue}
 import play.api.mvc._
 import lib._
 
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 
 object PlanApi extends Controller with PanDomainAuthActions with WorkflowApi {
 
@@ -51,12 +52,44 @@ object PlanApi extends Controller with PanDomainAuthActions with WorkflowApi {
 
   def addPlannedItem() = APIAuthAction { implicit request =>
     Response(for {
-      jsValue <- readJsonFromRequest(request.body).right
+      jsValue <- {
+        println(request.body)
+        readJsonFromRequest(request.body).right
+      }
       plannedItem <- extract[PlannedItem](jsValue.data).right
-      itemId <- queryDataToResponse(PlannedItemDB.upsert(plannedItem.data), "Could not add plan item").right
+      itemId <- queryDataToResponse(PlannedItemDB.insert(plannedItem.data), "Could not add plan item").right
     } yield {
       itemId
     })
+  }
+
+  def extractFromJson(fieldName: String, jsValueData: JsValue) = {
+    fieldName match {
+      case "title" => extract[String](jsValueData)
+      case "byLine"|"notes" => extract[Option[String]](jsValueData)
+      case "newsList"|"bundleId" => extract[Long](jsValueData)
+      case "priority" => extract[Int](jsValueData)
+      case "plannedDate"|"created" =>
+        // this could possibly be improved by replacing with a custom reads
+        val dateString = extract[String](jsValueData).right.map { d => d.data}.right.getOrElse("")
+        Try(new DateTime(dateString)) match {
+          case Success(dt) => Right(ApiSuccess(dt))
+          case Failure(msg) => Left(ApiError("Could not parse date", "Could not parse date", 500, "Error"))
+        }
+
+      case "hasSpecificTime"|"bucketed" => extract[Boolean](jsValueData)
+      case _ => Left(ApiError("Invalid field name", "Invalid field name", 500, "Error"))
+    }
+  }
+
+  def patchPlannedItem(id: Long, fieldName: String) = Action { implicit request =>
+    Response(for {
+      jsValue <- readJsonFromRequest(request.body).right
+      newFieldValue <- extractFromJson(fieldName, jsValue.data \ "data").right
+      itemId <- queryDataToResponse(PlannedItemDB.update(id, fieldName, newFieldValue.data), "Could not update plan item").right
+    } yield {
+        itemId
+      })
   }
 
   def deletePlannedItem() = APIAuthAction { implicit request =>
