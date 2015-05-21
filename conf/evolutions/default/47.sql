f831a95caba373c8ceb2964806fbe485f01c030c
Chris Finch <chris.finch@guardian.co.uk>
2015-05-21 16:07:05 +0100

    plan_item bundleId > Foreign Key

--- conf/evolutions/default/47.sql
+++ conf/evolutions/default/47.sql
@@ -1,21 +1,13 @@
 # --- !Ups
 
-CREATE TABLE plan_item
+CREATE TABLE bundle
 (
     pk SERIAL NOT NULL PRIMARY KEY,
-    title TEXT NOT NULL,
-    news_list INTEGER NOT NULL REFERENCES news_list (pk),
-    planned_date TIMESTAMP NOT NULL,
-    by_line TEXT,
-    notes TEXT,
-    bundle_id INTEGER NOT NULL,
-    created TIMESTAMP NOT NULL ,
-    priority INTEGER NOT NULL,
-    bucketed BOOLEAN NOT NULL,
-    has_specific_time BOOLEAN NOT NULL,
-    composer_id CHARACTER VARYING(32) UNIQUE
+    title TEXT NOT NULL
 );
+
+INSERT INTO bundle VALUES (0, 'No bundle');
 
 # --- !Downs
 
-DROP table plan_item
+DROP table bundle
