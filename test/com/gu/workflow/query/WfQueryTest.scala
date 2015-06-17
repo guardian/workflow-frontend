296c155e641e689f7d2cf8653ceac44cb1082cf9
lindseydew <lindsey.dew@guardian.co.uk>
2015-06-17 16:00:28 +0100

    rework the query param reads

---
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -0,0 +1,15 @@
+package com.gu.workflow.query
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class WfQueryTest extends FreeSpec with  Matchers {
+
+
+
+
+  "fromRequest" - {
+    "should transform query params into a wfquery object"
+
+  }
+
+}
