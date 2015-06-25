cf8a81d52f52d24bce3005b4011589ddcd26710d
lindseydew <lindsey.dew@guardian.co.uk>
2015-06-25 14:24:50 +0100

    add client side config

--- app/controllers/Support.scala
+++ app/controllers/Support.scala
@@ -1,14 +1,22 @@
 package controllers
 
+import com.gu.workflow.lib.{ClientLog, ClientMessageLoggable}
 import play.api.Logger
+import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
 
 object Support extends Controller {
 
   def logger = Action { req =>
-    Logger.info("******* DO I GET HIT*****")
-    println(req.body.asJson)
+    (for {
+        js <- req.body.asJson
+        msg <- js.validate[ClientLog].asOpt
+    } yield {
+      ClientMessageLoggable.logClientMessage(msg)
+    }).getOrElse {
+      Logger.info(s"unregonised message ${req.body}")
+    }
     NoContent
   }
 
