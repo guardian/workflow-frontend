18bb0454d5af72dbe053e47c90db535df0d4800c
lindseydew <lindsey.dew@guardian.co.uk>
2015-08-03 15:14:39 +0100

    extend inserting items to datastore

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -7,17 +7,18 @@ import org.scalatest._
 class WorkflowSpec extends WorkflowIntegrationSuite with Inside {
   s"$host/api/content" should "show content in db" in {
     val expectedTitle = "Content Item"
-    val content = createContent(contentItem(expectedTitle))
+    val content = createContent(contentItem(defaultStub(title=expectedTitle)))
 
     val js: JsValue = getJs("api/content")
+    
     val actualTitle = ((js \ "stubs").apply(0) \ "title").validate[String].asOpt.get
 
     expectedTitle should equal(actualTitle)
   }
 
   s"$host/api/content?text=query" should "show filter results from api" in {
-    createContent(contentItem("Foo"))
-    createContent(contentItem("Bar"))
+    createContent(contentItem(defaultStub(title="Foo")))
+    createContent(contentItem(defaultStub(title="Bar")))
 
     val includedStubs = (getJs(s"api/content?text=Foo") \ "stubs").as[JsArray]
     val excludedStubs = (getJs(s"api/content?text=Bar") \ "stubs").as[JsArray]
---
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -0,0 +1,15 @@
+package com.gu.workflow.query
+
+import org.scalatest.{Matchers, FreeSpec}
+
+class WfQueryTest extends FreeSpec with Matchers {
+  "test should run"  - {
+    "I can run a test" in {
+      1 should equal (1)
+
+    }
+
+  }
+
+
+}
