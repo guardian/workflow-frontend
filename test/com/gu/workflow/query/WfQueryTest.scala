80216c0f77b5aface1ac94b4bd84247acaade076
lindseydew <lindsey.dew@guardian.co.uk>
2015-08-03 15:26:21 +0100

    sketch out execution flow of test

--- test/com/gu/workflow/query/WfQueryTest.scala
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -1,12 +1,21 @@
 package com.gu.workflow.query
 
+import models.Status
 import org.scalatest.{Matchers, FreeSpec}
 
 class WfQueryTest extends FreeSpec with Matchers {
-  "test should run"  - {
-    "I can run a test" in {
-      1 should equal (1)
+  "test query on status"  - {
+    //generate all status data.
+    //persist
 
+    "I can run a test" in {
+      val wfQuery = WfQuery(status=Nil)
+      //all results
+
+      val wfQueryOne = WfQuery(status=Seq(Status("Writers")))
+      //should get only writers back
+
+      val wfQueryTwo = WfQuery(status=Seq(Status("Writers"),Status("Desk")))
     }
 
   }
