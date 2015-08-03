cf788d0a71e96c24a940f649b7453e313d3f6905
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-03 15:41:18 +0100

    .

--- test/com/gu/workflow/query/WfQueryTest.scala
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -1,12 +1,35 @@
 package com.gu.workflow.query
 
+import test._
+import models.ContentItem
 import models.Status
 import org.scalatest.{Matchers, FreeSpec}
 
-class WfQueryTest extends FreeSpec with Matchers {
+class WfQueryTest extends FreeSpec with Matchers with WorkflowHelpers {
   "test query on status"  - {
+
     //generate all status data.
     //persist
+
+    def generateStatus(statuses: List[Status]): List[ContentItem] = {
+      statuses.map { st =>
+        contentItem(defaultStub(), Some(defaultWorkflow(status = st)))
+      }
+    }
+
+    val testData = generateStatus(
+      ("Writers" ::
+         "Desk" ::
+         "Production Editor" ::
+         "Subs" ::
+         "Revise" ::
+         "Final" ::
+         "Hold" ::
+         Nil) map (Status(_)))
+
+    testData.foreach(createContent(_))
+
+    println(testData)
 
     "I can run a test" in {
       val wfQuery = WfQuery(status=Nil)
@@ -16,6 +39,8 @@ class WfQueryTest extends FreeSpec with Matchers {
       //should get only writers back
 
       val wfQueryTwo = WfQuery(status=Seq(Status("Writers"),Status("Desk")))
+
+      assert(1 == 1)
     }
 
   }
