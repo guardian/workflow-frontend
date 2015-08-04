7e7112a4e13b495038126ddfb9422cdef8719a6f
lindseydew <lindsey.dew@guardian.co.uk>
2015-08-04 11:22:56 +0100

    add test for multiple params

--- test/com/gu/workflow/query/WfQueryTest.scala
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -3,7 +3,7 @@ package com.gu.workflow.query
 import lib.PostgresDB
 import org.joda.time.DateTime
 import test._
-import models.{Section, DashboardRow, ContentItem, Status}
+import models._
 import org.scalatest.{Matchers, FreeSpec}
 
 class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite  with Matchers{
@@ -24,18 +24,15 @@ class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite  with Matchers{
       "Hold" ::
       Nil) map (Status(_)))
 
+
   "test query on status"  - {
 
     //generate all status data.
     //persist
 
-
-
     "One parameter set for a status" in {
       val dataInserted = testData.map(createContent(_)).flatten
-
       val filteredDataInserted = dataInserted.filter(_.wcOpt.map(wc => wc.status) == Some(Status("Writers")))
-
       val wfQuery = WfQuery(status=Seq(Status("Writers")))
       val list: List[DashboardRow] = PostgresDB.getContent(wfQuery)
       val dataQueried = list.map(DashboardRow.toContentItem(_))
@@ -45,18 +42,28 @@ class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite  with Matchers{
   }
 
   "second test" - {
-
     "No parameter set for a status" in {
       val dataInserted = testData.map(createContent(_)).flatten
-      println(s"DATA INSERTED ${dataInserted}")
       val wfQuery = WfQuery()
       val list: List[DashboardRow] = PostgresDB.getContent(wfQuery)
       val dataQueried = list.map(DashboardRow.toContentItem(_))
-      println(s"DATA QUERIED ${dataQueried}")
-      dataInserted should equal (dataQueried)
+      dataInserted should equal(dataQueried)
+    }
+  }
+
+  "third test" - {
+    "Multiple parameters set for status" in {
+      val dataInserted = testData.map(createContent(_)).flatten
+      val wfQuery = WfQuery(status=Seq(Status("Writers"), Status("Desk")))
+      val filteredDataInserted = dataInserted.filter(d => d.wcOpt.map(wc => wc.status) == Some(Status("Writers")) || d.wcOpt.map(wc => wc.status) == Some(Status("Desk")))
+      val list: List[DashboardRow] = PostgresDB.getContent(wfQuery)
+
+      val dataQueried = list.map(DashboardRow.toContentItem(_))
+
+      filteredDataInserted should equal (dataQueried)
+
     }
 
   }
-
 
 }
