f7a68fc0636d67ea36b86069a75662ee99a1861a
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-04 12:21:48 +0100

    initial attempt to create matchers to abstract a filter test

---
+++ test/com/gu/workflow/query/FilterTest.scala
@@ -0,0 +1,37 @@
+package com.gu.workflow.query
+
+import lib.PostgresDB
+import models.ContentItem
+import org.scalatest.Matchers
+
+trait FilterTestOps extends Matchers {
+
+  case class FilterTest(p: (ContentItem) => Boolean) {
+    def splitTestData(testData: List[ContentItem]): (List[ContentItem], List[ContentItem]) = {
+      (Nil, Nil)
+    }
+
+    def compareTo(testData: List[ContentItem], dbResult: DBResult) = {
+      val (testIn, testOut) = splitTestData(testData)
+      dbResult.results should contain theSameElementsAs (testIn)
+      dbResult.rest should contain theSameElementsAs (testOut)
+    }
+  }
+
+  case class DBResult(query: WfQuery, inputData: List[ContentItem]) {
+    val results = PostgresDB.getContent(query)
+    val rest = inputData diff results
+  }
+
+}
+// trait FilterTestOps extends Matchers {
+//   def compare(filter: FilterTest, res: DBResult, testData: List[ContentItem]) = {
+//               testData: List[ContentItem],
+//               dbAll: List[ContentItem],
+//               dbIn: List[ContentItem]) = {
+//     val (testIn, testOut) = filter.splitTestData(testData)
+//     val dbOut = dbAll diff dbIn
+
+//     dbIn should contain theSameElementsAs (testIn)
+//   }
+// }
--- test/com/gu/workflow/query/WfQueryTest.scala
+++ test/com/gu/workflow/query/WfQueryTest.scala
@@ -6,7 +6,7 @@ import test._
 import models._
 import org.scalatest.{Matchers, FreeSpec}
 
-class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite  with Matchers{
+class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite with FilterTestOps with Matchers{
 
   def generateStatus(statuses: List[Status]): List[ContentItem] = {
     statuses.map { st =>
@@ -32,11 +32,17 @@ class WfQueryTest extends FreeSpec with WorkflowIntegrationSuite  with Matchers{
 
     "One parameter set for a status" in {
       val dataInserted = testData.map(createContent(_)).flatten
-      val filteredDataInserted = dataInserted.filter(_.wcOpt.map(wc => wc.status) == Some(Status("Writers")))
-      val wfQuery = WfQuery(status=Seq(Status("Writers")))
-      val list: List[DashboardRow] = PostgresDB.getContent(wfQuery)
-      val dataQueried = list.map(DashboardRow.toContentItem(_))
-      filteredDataInserted should contain theSameElementsAs (dataQueried)
+      val oneStatus = FilterTest { content => true }
+      val query = WfQuery(status=Seq(Status("Writers")))
+
+      oneStatus.compareTo(dataInserted, DBResult(query, dataInserted))
+
+      // val dataInserted = testData.map(createContent(_)).flatten
+      // val filteredDataInserted = dataInserted.filter(_.wcOpt.map(wc => wc.status) == Some(Status("Writers")))
+      // val wfQuery =
+      // val list: List[DashboardRow] = PostgresDB.getContent(wfQuery)
+      // val dataQueried = list.map(DashboardRow.toContentItem(_))
+      // filteredDataInserted should contain theSameElementsAs (dataQueried)
     }
 
   }
