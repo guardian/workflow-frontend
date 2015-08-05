add7fea75a44f3118d1d08f73deb16f1f7a99f28
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-05 15:07:34 +0100

    add a double entry test

--- test/com/gu/workflow/query/TextSearchTest.scala
+++ test/com/gu/workflow/query/TextSearchTest.scala
@@ -20,22 +20,31 @@ class TextSearchTest extends FreeSpec with WorkflowIntegrationSuite with Matcher
       query should selectSameResultsAs (FilterTest(f, dataInserted))
     }
 
-  def testData: List[ContentItem] = List(
-    contentItem(defaultStub(title = "This has the magic word, xyzzy, in it"),
-                Some(defaultWorkflow())),
-    contentItem(defaultStub().copy(note = Some("This has the magic word, xyzzy, in it")),
-                Some(defaultWorkflow())),
-    contentItem(defaultStub(), Some(defaultWorkflow()))
-  )
+  val matchStr = "xyzzy"
+
+  val testData: List[ContentItem] = {
+    val stringWithMatch = s"This has the magic word, ${matchStr}, in it"
+    List(
+      // just title
+      contentItem(defaultStub(title = stringWithMatch), Some(defaultWorkflow())),
+      // just note
+      contentItem(defaultStub().copy(note = Some(stringWithMatch)), Some(defaultWorkflow())),
+      // match in both title and note
+      contentItem(defaultStub(title = stringWithMatch).copy(note = Some(stringWithMatch)),
+                  Some(defaultWorkflow())),
+      // no match
+      contentItem(defaultStub(), Some(defaultWorkflow()))
+    )
+  }
 
   "TextSearch" - {
 
     "empty should return everything" in doTest(noFilter, WfQuery(text = None))
 
     "with should match against correct fields" in {
-      val targetString = "xyzzy"
-      doTest(findTextOp(targetString), WfQuery(text = Some(targetString)))
+      doTest(findTextOp(matchStr), WfQuery(text = Some(matchStr)))
     }
+
  }
 
 }
