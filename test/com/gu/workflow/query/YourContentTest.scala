5902c9e4c41f911ab08d0ae78fcca52db969aefb
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-06 15:48:59 +0100

    initial test for assigned to email

---
+++ test/com/gu/workflow/query/YourContentTest.scala
@@ -0,0 +1,31 @@
+package com.gu.workflow.query
+
+import lib.PostgresDB
+import models.ContentItem
+import FilterTestOps._
+import test._
+
+import org.scalatest.{Matchers, FreeSpec}
+
+class YourContentTest extends FreeSpec with WorkflowIntegrationSuite with Matchers {
+
+  val testEmail = "guardian@example.com"
+
+  val assignedToField: FieldGetter[Option[String]] = _.stub.assigneeEmail
+
+  def assignedToTest(pattern: String) = fieldTest(_.stub.assigneeEmail,
+                                                  optTest[String](_ == pattern))
+
+  val testData: Content = List(
+    contentItem(defaultStub(), Some(defaultWorkflow())),
+    contentItem(defaultStub().copy(assigneeEmail = Some(testEmail)),
+                Some(defaultWorkflow()))
+    )
+
+  "YourContent query" - {
+    "should find correctly assigned content" in withTestData(testData) { insertedData =>
+      (WfQuery(assignedToEmail = List(testEmail + "x"))
+         should selectSameResultsAs (FilterTest(assignedToTest(testEmail), insertedData)))
+    }
+  }
+}
