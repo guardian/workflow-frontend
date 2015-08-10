196b00c0a1db5fdde7d374c58e051c0eaf2f5bd3
Paul Roberts <paul.roberts@guardian.co.uk>
2015-08-10 17:09:04 +0100

    interim save

--- test/lib/Suite.scala
+++ test/lib/Suite.scala
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebDriver
 
 import com.gu.workflow.test.CommonDBIntegrationSuite
 
-trait WorkflowIntegrationSuite extends Suite with CommonDBIntegrationSuite with OneServerPerSuite with BeforeAndAfterAll with BeforeAndAfterEach with Matchers with ShouldMatchers with Http with WorkflowHelpers {
+trait WorkflowIntegrationSuite extends Suite with CommonDBIntegrationSuite with OneServerPerSuite with Matchers with ShouldMatchers with Http with WorkflowHelpers {
   implicit override lazy val app: FakeApplication = FakeApplication(
     additionalConfiguration = Config.appConfig)
 
@@ -24,39 +24,6 @@ trait WorkflowIntegrationSuite extends Suite with CommonDBIntegrationSuite with 
     connection.responseCode should be (expectedCode)
 
     Json.parse(connection.body)
-  }
-
-  def withTestData(testData: List[ContentItem])(f: (List[ContentItem]) => Unit) =
-    f(testData.map(createContent(_)).flatten)
-
-  def withCollaboratorTestData(testData: List[ContentItemWithCollaborators])
-                              (f: (List[ContentItem]) => Unit) = {
-    val content = testData.map(_.contentItem)
-    val dataInserted = content.map(createContent(_)).flatten
-    testData.foreach(item => addCollaborators(item.contentItem, item.collaborators))
-    f(dataInserted)
-  }
-
-  override def beforeAll() {
-    val props = System.getProperties();
-    props.setProperty("config.resource", "application.ci.conf");
-
-    DatabaseManager.destroy
-    DatabaseManager.create
-  }
-
-  override def afterAll() {
-    if(Config.dropDB) DatabaseManager.destroy
-    else println("Not dropping database")
-  }
-
-  override def beforeEach() {
-
-    //todo - fix the clear content
-    DatabaseManager.clearContent
-
-//    DatabaseManager.destroy
-//    DatabaseManager.create
   }
 
   val host = Config.appConfig("host")
--- test/lib/WorkflowHelpers.scala
+++ test/lib/WorkflowHelpers.scala
@@ -12,27 +12,4 @@ import play.api.db.slick.DB
 import play.api.Play.current
 
 trait WorkflowHelpers {
-
-  import scala.language.implicitConversions
-
-  case class ContentItemWithCollaborators(contentItem: ContentItem, collaborators: List[User] = Nil) {
-    def withCollaborators(updatedCollabs: User*) = this.copy(collaborators = updatedCollabs.toList)
-  }
-
-  implicit def hasCollaborators(c: ContentItem): ContentItemWithCollaborators =
-    ContentItemWithCollaborators(c)
-
-  def createContent(item: ContentItem): Option[ContentItem] = {
-    val stubId = PostgresDB.createContent(item)
-    stubId.fold(err => None, apiSucc => Some(ContentItem(item.stub.copy(Some(apiSucc.data)), item.wcOpt)))
-  }
-
-  // returns 'content' for chaining
-  def addCollaborators(content: ContentItem, collaborators: List[User]) =
-    DB.withSession { implicit session =>
-      content.wcOpt.map( c =>
-        CommonDB.addCollaborators(content.wcOpt, collaborators.toList, c.composerId)
-      )
-      content
-    }
 }
