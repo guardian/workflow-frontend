0b83a0f85e4324ee99d37c6db67c85443277575f
james@jamespamplin.com <james@jamespamplin.com>
2015-08-28 17:12:35 +0100

    Fix viewer config

--- app/lib/PrototypeConfiguration.scala
+++ app/lib/PrototypeConfiguration.scala
@@ -9,6 +9,7 @@ case class PrototypeConfiguration(
                                    googleClientId: String,
                                    googleClientSecret: String,
                                    host: String,
+                                   viewerUrl: String,
                                    presenceUrl: String,
                                    presenceClientLib: String,
                                    preferencesUrl: String,
@@ -40,7 +41,7 @@ object PrototypeConfiguration {
         logStashHost <- Config.getConfigString("logging.logstash.host").right
         logStashPort <- Config.getConfigInt("logging.logstash.port").right
         logStashEnabled <- Config.getConfigBoolean("logging.logstash.enabled").right
-      } yield PrototypeConfiguration(composerUrl, composerRestorerUrl, googleClientId, googleClientSecret, host, presenceUrl, presenceClientLib, preferencesUrl, incopyExportUrl, LogStashConf(logStashHost, logStashPort, logStashEnabled)))
+      } yield PrototypeConfiguration(composerUrl, composerRestorerUrl, googleClientId, googleClientSecret, host, viewerUrl, presenceUrl, presenceClientLib, preferencesUrl, incopyExportUrl, LogStashConf(logStashHost, logStashPort, logStashEnabled)))
     configEit.fold(error => {
       Logger.error(s"could not instantiate Prototype Configuration ${error}")
       sys.error(error)
