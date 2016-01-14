4f1a5064c06cae0023043f12ae1f7c6573e8075a
lindseydew <lindsey.dew@guardian.co.uk>
2016-01-14 17:29:15 +0000

    add tests for delete, update

--- test/com/gu/workflow/plan/DayNoteDB$Test.scala
+++ test/com/gu/workflow/plan/DayNoteDB$Test.scala
@@ -5,6 +5,7 @@ import com.gu.workflow.query.WfQuery
 import com.gu.workflow.test.CommonDBIntegrationSuite
 import com.gu.workflow.test.lib.TestData._
 import models.ContentItem
+import org.joda.time.DateTime
 import org.scalatest.{FreeSpec, Matchers}
 
 
@@ -14,7 +15,9 @@ class DayNoteDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matcher
 
   "Should retrieve a day note inserted" - {
     "get day note by id" in {
-      val dayNoteFromDB = createDayNote(dayNote)
+      val dayNote = generateDayNote()
+      val newsList = generateNewsList()
+      val dayNoteFromDB = createDayNote(dayNote, newsList)
       DayNoteDB.getDayNoteById(dayNoteFromDB.id) should equal (Some(dayNoteFromDB))
     }
 
@@ -22,4 +25,37 @@ class DayNoteDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matcher
       DayNoteDB.getDayNoteById(4L) should equal (None)
     }
   }
+
+  "Should delete a day note" - {
+    "delete day not that exists" in {
+      val dayNote = generateDayNote()
+      val newsList = generateNewsList()
+      val dayNoteFromDB = createDayNote(dayNote, newsList)
+      DayNoteDB.deleteById(dayNoteFromDB.id) should equal (Some(dayNoteFromDB.id))
+    }
+
+    "return none if item doesn't exist" in {
+      DayNoteDB.deleteById(3L) should equal (None)
+    }
+  }
+
+  "Should update a field" - {
+    "update a note" in {
+      val dayNote = generateDayNote()
+      val newsList = generateNewsList()
+      val dayNoteFromDB = createDayNote(dayNote, newsList)
+      DayNoteDB.updateDayNote(dayNoteFromDB.id, "note", "notevalue")
+      DayNoteDB.getDayNoteById(dayNoteFromDB.id).map(_.note) should equal (Some("notevalue"))
+    }
+
+    "update a day" in {
+      val dayNote = generateDayNote()
+      val newsList = generateNewsList()
+      val dayNoteFromDB = createDayNote(dayNote, newsList)
+      val now = DateTime.now()
+      DayNoteDB.updateDayNote(dayNoteFromDB.id, "day", now)
+      DayNoteDB.getDayNoteById(dayNoteFromDB.id).map(_.day) should equal (Some(now))
+    }
+  }
+
 }
