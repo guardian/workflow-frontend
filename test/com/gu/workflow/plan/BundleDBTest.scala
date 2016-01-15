3e3300cf25eb745c8d6f7b8e8ecbe76a3ad1d731
lindseydew <lindsey.dew@guardian.co.uk>
2016-01-15 18:01:27 +0000

    add test coverage of bundles

--- test/com/gu/workflow/plan/BundleDBTest.scala
+++ test/com/gu/workflow/plan/BundleDBTest.scala
@@ -4,16 +4,55 @@ import com.gu.workflow.db.{BundleDB, DayNoteDB}
 import com.gu.workflow.test.CommonDBIntegrationSuite
 import com.gu.workflow.test.lib.TestData._
 import models.Bundle
+import org.joda.time.DateTime
 import org.scalatest.{Matchers, FreeSpec}
 
 class BundleDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matchers {
 
-  "Should retrieve a day bundle inserted" - {
+  "Should query on bundles" in withBundleTestData(generateBundles()) { dataInserted =>
+    BundleDB.getBundles() should equal (Some(dataInserted.sortBy(b => b.id)))
+  }
+
+  "Should retrieve a bundle inserted" - {
     "get bundle by id" in {
       val bundle = generateBundle()
       val bundleFromDB = createBundle(bundle)
       BundleDB.getBundleById(bundleFromDB.id) should equal (Some(bundleFromDB))
     }
+
+    "should return none if not inserted" in {
+      BundleDB.getBundleById(1000L) should equal (None)
+    }
   }
+
+  "Should delete a bundle" - {
+    "delete bundle that exists" in {
+      val bundle = generateBundle()
+      val bundleFromDB = createBundle(bundle)
+      BundleDB.deleteBundle(bundleFromDB) should equal (Some(bundleFromDB.id))
+      BundleDB.getBundleById(bundleFromDB.id) should equal (None)
+
+    }
+
+    "return none if item doesn't exist" in {
+      val dayNote = generateDayNote()
+      DayNoteDB.deleteDayNote(dayNote) should equal (None)
+    }
+  }
+
+  "Should update a field" - {
+    "update a title" in {
+      val bundle = generateBundle()
+      val bundleFromDB = createBundle(bundle)
+      BundleDB.update(bundleFromDB.id, "title", "asdfh")
+      BundleDB.getBundleById(bundleFromDB.id).map(_.title) should equal (Some("asdfh"))
+    }
+
+    "update a day note that doesn't exist" in {
+      val bundle = generateBundle()
+      BundleDB.update(bundle.id, "title", "asdfh") should equal (None)
+    }
+  }
+
 
 }
