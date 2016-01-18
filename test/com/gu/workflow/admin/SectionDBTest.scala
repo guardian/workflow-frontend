2bf4ebfa4438054186b64c0bc1b91fdedf3af3e7
lindseydew <lindsey.dew@guardian.co.uk>
2016-01-18 16:55:17 +0000

    add tests for sections

--- test/com/gu/workflow/admin/SectionDBTest.scala
+++ test/com/gu/workflow/admin/SectionDBTest.scala
@@ -1,8 +1,33 @@
 package com.gu.workflow.admin
 
+import com.gu.workflow.db.{SectionDB, DeskDB}
 import com.gu.workflow.test.CommonDBIntegrationSuite
+import com.gu.workflow.test.lib.TestData._
 import org.scalatest.{Matchers, FreeSpec}
 
 class SectionDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matchers {
+
+  "Should retrieve sections" in withSectionsTestData(generateSections()) { dataInserted =>
+    SectionDB.sectionList.map(_.name) should equal("Cities" :: "Technology" :: "Dev" :: dataInserted.map(_.name))
+  }
+
+  "Should insert a section with a unique name" in {
+    val section = generateSection()
+    SectionDB.upsert(section) should equal(1)
+  }
+
+  "Should do nothing if the upsert is called on a section with the same name" in {
+    val section = generateSection()
+    SectionDB.upsert(section) should equal(1)
+    SectionDB.upsert(section) should equal(0)
+
+  }
+
+  "Should remove a section" in {
+    val section = generateSection()
+    val sectionFromDB = createSection(section)
+    SectionDB.remove(section) should equal (1)
+    SectionDB.getByName(sectionFromDB.name) should equal (None)
+  }
 
 }
--- test/com/gu/workflow/admin/SectionDeskMappingDBTest.scala
+++ test/com/gu/workflow/admin/SectionDeskMappingDBTest.scala
@@ -3,6 +3,8 @@ package com.gu.workflow.admin
 import com.gu.workflow.test.CommonDBIntegrationSuite
 import org.scalatest.{Matchers, FreeSpec}
 
-class SectionDeskMappingDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matchers  {
+class SectionDeskMappingDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matchers {
+
+  
 
 }
