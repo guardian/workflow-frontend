64d170ec1c1ecb9068001c92f9207a30165d5f8b
lindseydew <lindsey.dew@guardian.co.uk>
2016-01-19 16:47:27 +0000

    remove the news list test skeleton

--- test/com/gu/workflow/admin/NewsListDBTest.scala
+++
@@ -1,8 +0,0 @@
-package com.gu.workflow.admin
-
-import com.gu.workflow.test.CommonDBIntegrationSuite
-import org.scalatest.{Matchers, FreeSpec}
-
-class NewsListDBTest extends FreeSpec with CommonDBIntegrationSuite  with Matchers {
-
-}
