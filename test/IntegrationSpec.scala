64621052281dfc120700fc5c00819b0c21fe2c6d
lindseydew <lindsey.dew@guardian.co.uk>
2016-02-24 14:11:29 +0000

    use the content item json writes to create stub

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -45,7 +45,7 @@ class WorkflowSpec extends FreeSpec  with  WorkflowIntegrationSuite with Inside 
 
   s"$host/api/stubs" - {
     "create a stub" in {
-      val stubJson = createContentItemJson(randomStub, generateSection)
+      val stubJson = Json.toJson(randomStub)
       val res = postJS(s"api/stubs", stubJson)
       val id = (res \ "data" \ "stubId").validate[Long]
       id.isSuccess should be (true)
@@ -64,7 +64,7 @@ class WorkflowSpec extends FreeSpec  with  WorkflowIntegrationSuite with Inside 
 
     "should fail if stubId defines a composerId and workflow content is not defined" in {
       val stubWithComposerId = ContentItem(randomStub.stub.copy(composerId=Some("composerId")), None)
-      val res = postJS(s"api/stubs", Json.toJson(stubWithComposerId, 400)
+      val res = postJS(s"api/stubs", Json.toJson(stubWithComposerId), 400)
       val errorMessage = (res \ "error" \ "message").validate[String]
       errorMessage.fold(_ => fail("error message should be defined"), message =>
         message should equal ("JsonParseError")
