4dcf88eb8936a33672b4cd8599033be8e234f63b
lindseydew <lindsey.dew@guardian.co.uk>
2016-02-24 11:01:02 +0000

    integration test posting new content
    Conflicts:
    	prototype/app/lib/Response.scala
    	prototype/test/com/gu/workflow/lib/DBToAPIResponseTest.scala

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,8 +1,6 @@
 package test
 
-import models.ContentItem
-import models.Status
-import models.Stub
+import models.{Section, ContentItem, Status, Stub}
 import org.joda.time.DateTime
 import org.scalactic.Equality
 import play.api.libs.json._
@@ -42,4 +40,43 @@ class WorkflowSpec extends FreeSpec  with  WorkflowIntegrationSuite with Inside 
       excludedTitle should equal("Bar")
     }
   }
+
+  def createContentItemJson(c: ContentItem, section: Section): JsValue = {
+    Json.toJson(c).as[JsObject] ++ Json.obj("section" -> Json.toJson(section).as[JsObject])
+  }
+
+  s"$host/api/stubs" - {
+    "create a stub" in {
+      val stubJson = createContentItemJson(randomStub, generateSection)
+      val res = postJS(s"api/stubs", stubJson)
+      val id = (res \ "data" \ "stubId").validate[Long]
+      id.isSuccess should be (true)
+    }
+
+    "create a stub and workflow content" - {
+      val ci = randomStubAndWC
+      val contentJson = createContentItemJson(ci, generateSection)
+      val res = postJS(s"api/stubs", contentJson)
+      val id = (res \ "data" \ "stubId").validate[Long]
+      val composerId = (res \ "data" \ "composerId").validate[String]
+      id.isSuccess should be (true)
+      composerId.fold(_ => fail("composerId should be defined"), cId =>
+        cId should equal (ci.wcOpt.map(_.composerId))
+      )
+    }
+
+    "should fail if stubId defines a composerId and workflow content is not defined" in {
+      val stubWithComposerId = ContentItem(randomStub.stub.copy(composerId=Some("composerId")), None)
+      val stubJson = createContentItemJson(stubWithComposerId, generateSection)
+      val res = postJS(s"api/stubs", stubJson, 400)
+      val errorMessage = (res \ "error" \ "message").validate[String]
+      errorMessage.fold(_ => fail("error message should be defined"), message =>
+        message should equal ("JsonParseError")
+      )
+    }
+  }
+
+
+
+
 }
--- test/com/gu/workflow/lib/DBToAPIResponseTest.scala
+++ test/com/gu/workflow/lib/DBToAPIResponseTest.scala
@@ -7,8 +7,10 @@ import org.scalatest.{Matchers, FreeSpec}
 class DBToAPIResponseTest  extends FreeSpec with Matchers {
   "updateRes" - {
     "should return not found error if returned row is 0" in {
-      DBToAPIResponse.updateRes(3L, 0).fold(apiError =>
-        apiError.message should equal ("NotFound"),
+      DBToAPIResponse.updateRes(3L, 0).fold(apiError => {
+        apiError.message should equal("UpdateError")
+        apiError.statusCode should equal(404)
+      },
         r => fail(s"expected left got ${r}")
       )
     }
@@ -41,20 +43,36 @@ class DBToAPIResponseTest  extends FreeSpec with Matchers {
       }, r => fail(s"expected left got ${r}"))
     }
 
-    "should return a 404 is stub row is not there" in {
+    "should return a 404 is stub row is not found" in {
       DBToAPIResponse.upsertContentResponse(Left(StubNotFound(3L))).fold(apiError => {
         apiError.statusCode should equal (404)
       }, r => fail(s"expected left got ${r}"))
     }
 
-    "should return a 200 if content is successful upserted" in {
-      DBToAPIResponse.upsertContentResponse(Left(StubNotFound(3L))).fold(apiError => {
+
+    "should return a 200 if content is successfully upserted" in {
+      DBToAPIResponse.upsertContentResponse(Right(ContentUpdate(1L, Some("id")))).fold(apiError => {
         fail(s"expected right got left ${apiError}")
       }, r => {
         r.statusCode should equal (200)
-        r.data.stubId should equal (3L)
+        r.data.stubId should equal (1L)
+        r.data.composerId should equal (Some("id"))
       })
+    }
+  }
 
+  "deleteResponse" - {
+    "should return a 404 is response if delete operation is none" in {
+      DBToAPIResponse.deleteResponse(4L, None).fold(apiError => {
+         apiError.statusCode should equal (404)
+      }, r => fail(s"expected left got r ${r}"))
+    }
+    "should return a 200 response if delete operation is defined" in {
+      DBToAPIResponse.deleteResponse(4L, Some(DeleteOp(4L, 0))).fold(apiError => {
+        fail(s"expected right got left ${apiError}")
+      }, r => {
+        r.statusCode should equal (200)
+      })
     }
 
   }
