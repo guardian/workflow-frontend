145d803c8dec227c0c353a9cccd58c8023e83cca
lindseydew <lindsey.dew@guardian.co.uk>
2016-02-24 10:11:26 +0000

    support section being passed as a string in stub reads

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -53,8 +53,7 @@ class WorkflowSpec extends FreeSpec  with  WorkflowIntegrationSuite with Inside 
 
     "create a stub and workflow content" - {
       val ci = randomStubAndWC
-      val contentJson = createContentItemJson(ci, generateSection)
-      val res = postJS(s"api/stubs", contentJson)
+      val res = postJS(s"api/stubs", Json.toJson(ci))
       val id = (res \ "data" \ "stubId").validate[Long]
       val composerId = (res \ "data" \ "composerId").validate[Option[String]]
       id.isSuccess should be (true)
@@ -65,8 +64,7 @@ class WorkflowSpec extends FreeSpec  with  WorkflowIntegrationSuite with Inside 
 
     "should fail if stubId defines a composerId and workflow content is not defined" in {
       val stubWithComposerId = ContentItem(randomStub.stub.copy(composerId=Some("composerId")), None)
-      val stubJson = createContentItemJson(stubWithComposerId, generateSection)
-      val res = postJS(s"api/stubs", stubJson, 400)
+      val res = postJS(s"api/stubs", Json.toJson(stubWithComposerId, 400)
       val errorMessage = (res \ "error" \ "message").validate[String]
       errorMessage.fold(_ => fail("error message should be defined"), message =>
         message should equal ("JsonParseError")
