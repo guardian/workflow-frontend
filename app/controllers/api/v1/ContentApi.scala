a7c0945f19c35907bd2310e133679ebdb7b0e548
lindseydew <lindsey.dew@guardian.co.uk>
2016-02-25 15:09:10 +0000

    remove swagger docs since were not using it

--- app/controllers/api/v1/ContentApi.scala
+++ app/controllers/api/v1/ContentApi.scala
@@ -1,26 +1,14 @@
 package controllers
 
-import javax.ws.rs.PathParam
-import com.gu.workflow.lib._
 import lib._
-import Response.Response
 import com.gu.workflow.db.{CommonDB}
-import com.gu.workflow.query.WfQuery
-import controllers.Api._
-import lib.OrderingImplicits._
 import models._
-import org.joda.time.DateTime
-import play.api.libs.json._
 import play.api.mvc._
-import com.wordnik.swagger.annotations._
-import com.wordnik.swagger.core.util.ScalaJsonUtil
-import models.Status
 import lib.DBToAPIResponse._
 
-import scala.util.{Try, Either}
+import scala.util.Try
 
 
-@Api(value = "/content", description = "Operations about content")
 object ContentApi extends Controller with PanDomainAuthActions with WorkflowApi {
 
   // can be hidden behind multiple auth endpoints
@@ -36,17 +24,7 @@ object ContentApi extends Controller with PanDomainAuthActions with WorkflowApi 
     NotImplemented
   }
 
-  @ApiOperation(
-    nickname = "contentById",
-    value = "Get content by either Stub or Composer ID",
-    response = classOf[Long],
-    httpMethod = "GET"
-  )
-  @ApiResponses(Array(
-    new ApiResponse(code = 404, message = "NotFound")
-  ))
-  def contentById(
-    @ApiParam(value = "ID of the content item to fetch (Stub ID or Composer ID)") @PathParam("id") id: String) =  CORSable(composerUrl) {
+  def contentById(id: String) =  CORSable(composerUrl) {
     APIAuthAction { request =>
       Try(id.toLong).toOption match {
         case Some(l) => contentByStubId(l)
@@ -75,19 +53,7 @@ object ContentApi extends Controller with PanDomainAuthActions with WorkflowApi 
 		Response(contentEither)
   }
 
-  @ApiOperation(
-    nickname = "createContentItem",
-    value = "Create new content item",
-    response = classOf[Long],
-    httpMethod = "POST"
-  )
-  @ApiResponses(Array(
-    new ApiResponse(code = 409, message = "Conflict"),
-    new ApiResponse(code = 400, message = "InvalidContentType"),
-    new ApiResponse(code = 400, message = "JsonParseError")
-  ))
-  @ApiImplicitParams(Array(
-    new ApiImplicitParam(value = "Content object", required = true, dataType = "ContentItem", paramType = "body")))
+
   def createContent() = CORSable(composerUrl) {
     APIAuthAction { implicit request =>
       Response(for {
@@ -104,16 +70,7 @@ object ContentApi extends Controller with PanDomainAuthActions with WorkflowApi 
     NotImplemented
   }
 
-  @ApiOperation(
-    nickname = "deleteContentItem",
-    value = "Delete new content item",
-    response = classOf[Long],
-    httpMethod = "DELETE"
-  )
-  @ApiResponses(Array(
-    new ApiResponse(code = 404, message = "ContentNotFound")
-  ))
-  def deleteContent(@ApiParam(value = "ID of the content item to delete") @PathParam("id") id: Long) = {
+  def deleteContent(id: Long) = {
     APIAuthAction { implicit request =>
       Response(for {
         stubId <- deleteResponse(id, PostgresDB.deleteContentByStubId(id)).right
