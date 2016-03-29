3b8388d161c4cfb993703b92b5f545feb172d47e
hmgibson23 <hugo.gibson@yahoo.co.uk>
2016-03-29 14:58:52 +0100

    Switch both gets out

--- app/controllers/api/v1/ContentApi.scala
+++ app/controllers/api/v1/ContentApi.scala
@@ -1,10 +1,14 @@
 package controllers
 
+import com.gu.workflow.api.PrototypeAPI
 import lib._
 import com.gu.workflow.db.{CommonDB}
 import models._
+import play.api.Logger
+import models.api.ApiResponseFt
 import play.api.mvc._
 import lib.DBToAPIResponse._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.util.Try
 
@@ -12,32 +16,28 @@ import scala.util.Try
 object ContentApi extends Controller with PanDomainAuthActions with WorkflowApi {
 
   def contentById(id: String) =  CORSable(composerUrl) {
-    APIAuthAction { request =>
+    APIAuthAction.async {
       Try(id.toLong).toOption match {
-        case Some(l) => contentByStubId(l)
-        case None => contentByComposerId(id)
+            case Some(l) => contentByStubId(l)
+            case None => contentByComposerId(id)
       }
     }
   }
 
-  def contentByStubId(id: Long): Result =  {
-    val contentOpt: Option[ContentItem] = PostgresDB.getContentById(id)
-
-    Response(contentOpt match {
-      case Some(contentItem) => Right(ApiSuccess(contentItem))
-      case None => Left(ApiErrors.notFound)
+  def contentByStubId(id: Long) =  {
+    ApiResponseFt[Option[ContentItem]](for {
+      item <- PrototypeAPI.getContentByStubId(id)
+    } yield {
+      item
     })
   }
 
   def contentByComposerId(id: String) =  {
-    val contentOpt: Option[ContentItem] = CommonDB.getContentByCompserId(id)
-
-    val contentEither = contentOpt match {
-      case Some(contentItem) =>
-        contentItem.stub.id.map { id => Right(ApiSuccess(contentItem))}.getOrElse(Left(ApiErrors.notFound))
-      case None => Left(ApiErrors.notFound)
-    }
-		Response(contentEither)
+    ApiResponseFt[Option[ContentItem]](for {
+      item <- PrototypeAPI.getContentByComposerId(id)
+    } yield {
+      item
+    })
   }
 
 
