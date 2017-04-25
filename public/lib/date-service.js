4618c8125c469d0ae71b0f088d4feb5db8db5767
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-04-25 13:50:59 +0100

    Temporary fix for wf last modified

--- public/lib/date-service.js
+++ public/lib/date-service.js
@@ -69,180 +69,187 @@ angular.module('wfDateService', ['wfLocationService'])
         function (wfLocationService, wfFormatDateTimeFilter) {
 
 
-            class DateParser {
+    class DateParser {
 
-                /**
-                 * Parses a Date from an input string.
-                 *
-                 * @param {string} input string to parse.
-                 * @param {string} locationKey
-                 *
-                 * @return {Date}
-                 */
-                parseDate(input, locationKey) {
-                    return this.localiseDateTime(
-                        this.normaliseDateString(input, locationKey),
-                        locationKey
-                    ).toDate();
-                }
+        /**
+         * Parses a Date from an input string.
+         *
+         * @param {string} input string to parse.
+         * @param {string} locationKey
+         *
+         * @return {Date}
+         */
+        parseDate(input, locationKey) {
+            return this.localiseDateTime(
+                this.normaliseDateString(input, locationKey),
+                locationKey
+            ).toDate();
+        }
 
-                /**
-                 * Normalises a date input string to YYYY-MM-DD HH:mm, accepting
-                 * natural language inputs such as "today", "tuesday next week 18:00"
-                 */
-                normaliseDateString(input, locationKey) {
-                    locationKey = locationKey || wfLocationService.getCurrentLocationKey();
+        /**
+         * Normalises a date input string to YYYY-MM-DD HH:mm, accepting
+         * natural language inputs such as "today", "tuesday next week 18:00"
+         */
+        normaliseDateString(input, locationKey) {
+            locationKey = locationKey || wfLocationService.getCurrentLocationKey();
 
-                    var parsed;
-                    if (moment.isMoment(input)) {
-                        parsed = input;
+            var parsed;
+            if (moment.isMoment(input)) {
+                parsed = input;
 
-                    } else {
-                        // Parse input using sugar.js catering for natural language, ie: "next week"
-                        parsed = moment(Date.create(input, getTimezoneLocaleForLocation(locationKey)));
-                    }
+            } else {
+                // Parse input using sugar.js catering for natural language, ie: "next week"
+                parsed = moment(Date.create(input, getTimezoneLocaleForLocation(locationKey)));
+            }
 
-                    if (!parsed.isValid()) {
-                        throw new Error('Could not parse date: ' + input);
-                    }
+            if (!parsed.isValid()) {
+                throw new Error('Could not parse date: ' + input);
+            }
 
-                    return parsed.format('YYYY-MM-DD HH:mm');
-                }
+            return parsed.format('YYYY-MM-DD HH:mm');
+        }
 
-                now() {
-                    return new Date();
-                }
+        now() {
+            return new Date();
+        }
 
-                /**
-                 * Retrieve a date range between two explicit dates.
-                 *
-                 * @returns {{from: Date, to: Date}}
-                 */
-                createRange(from, until) {
-                    return {
-                        from: moment(from).toDate(),
-                        until: moment(until).toDate()
-                    };
-                }
+        /**
+         * Retrieve a date range between two explicit dates.
+         *
+         * @returns {{from: Date, to: Date}}
+         */
+        createRange(from, until) {
+            return {
+                from: moment(from).toDate(),
+                until: moment(until).toDate()
+            };
+        }
 
-                /**
-                 * Retrieve a range from the start of a day, til the start of the next.
-                 *
-                 * @returns {{from: Date, to: Date}}
-                 */
-                createDayRange(day) {
-                    var dayStart = moment(day).startOf('day');
+        /**
+         * Retrieve a range from the start of a day, til the start of the next.
+         *
+         * @returns {{from: Date, to: Date}}
+         */
+        createDayRange(day) {
+            var dayStart = moment(day).startOf('day');
+
+            return this.createRange(dayStart, dayStart.clone().add(1, 'days'));
+        }
+
+        /**
+         * Parses a date range using simple natural language strings
+         * (eg: "tomorrow") and explicit standard date formatted date strings,
+         * such as in YYYY-MM-DD.
+         *
+         * @returns {{from: Date, to: Date}}
+         */
+        parseRangeFromString(input, locationKey) {
+
+            var now = this.localiseDateTime(this.now(), locationKey).clone();
+
+            if (!input) {
+                return {};
+            }
+            // Parses some natural language dates - doesn't use sugar as I'd like
+            // to remove it as a dependency one day as it modifies the global Date object
+            if (input == 'today') {
+                const migrationDate = moment.tz("2017-04-25 11:00", "Europe/London");
+                const fixedDate = moment.tz("2017-04-26 11:00", "Europe/London");
+                if (now > fixedDate) {
+                    return this.createDayRange(now);
+                } else {
+                    var dayStart = migrationDate;
 
                     return this.createRange(dayStart, dayStart.clone().add(1, 'days'));
                 }
+            } else if (input == 'tomorrow') {
+                return this.createDayRange(now.add(1, 'days'));
 
-                /**
-                 * Parses a date range using simple natural language strings
-                 * (eg: "tomorrow") and explicit standard date formatted date strings,
-                 * such as in YYYY-MM-DD.
-                 *
-                 * @returns {{from: Date, to: Date}}
-                 */
-                parseRangeFromString(input, locationKey) {
+            } else if (input == 'weekend') {
+                var weekendStart = now.day(6).startOf('day');
 
-                    var now = this.localiseDateTime(this.now(), locationKey).clone();
+                return this.createRange(weekendStart, weekendStart.clone().add(2, 'days'));
 
-                    if (!input) {
-                        return {};
-                    }
-                    // Parses some natural language dates - doesn't use sugar as I'd like
-                    // to remove it as a dependency one day as it modifies the global Date object
-                    if (input == 'today') {
-                        return this.createDayRange(now);
+            } else if (input == 'yesterday') {
 
-                    } else if (input == 'tomorrow') {
-                        return this.createDayRange(now.add(1, 'days'));
+                return this.createDayRange(now.subtract(1, 'days'));
 
-                    } else if (input == 'weekend') {
-                        var weekendStart = now.day(6).startOf('day');
+            } else if (input === 'last24') {
 
-                        return this.createRange(weekendStart, weekendStart.clone().add(2, 'days'));
+                return this.createRange(now.clone().subtract(24, 'hours'), now);
 
-                    } else if (input == 'yesterday') {
+            } else if (input === 'last48') {
 
-                        return this.createDayRange(now.subtract(1, 'days'));
+                return this.createRange(now.clone().subtract(48, 'hours'), now);
 
-                    } else if (input === 'last24') {
+            } else {
+                var parsed = this.localiseDateTime(input, locationKey);
 
-                        return this.createRange(now.clone().subtract(24, 'hours'), now);
-
-                    } else if (input === 'last48') {
-
-                        return this.createRange(now.clone().subtract(48, 'hours'), now);
-
-                    } else {
-                        var parsed = this.localiseDateTime(input, locationKey);
-
-                        if (!parsed.isValid()) {
-                            throw new Error('Could not parse date: ' + input);
-                        }
-
-                        return this.createDayRange(parsed);
-                    }
+                if (!parsed.isValid()) {
+                    throw new Error('Could not parse date: ' + input);
                 }
 
-                /**
-                 * Retrieve either string representation of 'day', or day object
-                 *
-                 * @returns {String, or <Date>}
-                 */
-                parseQueryString(date) {
-                    if (!date) return undefined;
-                    if (date === 'today' || date === 'tomorrow' || date === 'weekend') {
-                        return date;
-                    }
-                    else if (moment(date, ["YYYY-MM-DD"]).isValid()) {
-                        return moment(date, ["YYYY-MM-DD"]);
-                    }
-                }
+                return this.createDayRange(parsed);
+            }
+        }
 
-                /**
-                 * Formats date to string if passed a date object, otherwise return what is passsed
-                 *
-                 * @returns {String, or <Date>}
-                 */
-                setQueryString(date) {
-                    if (!date) return undefined;
-                    var dateFormat = wfFormatDateTimeFilter(date, "YYYY-MM-DD");
-                    if (dateFormat !== 'Invalid date') {
-                        return dateFormat;
-                    }
-                    else return date;
+        /**
+         * Retrieve either string representation of 'day', or day object
+         *
+         * @returns {String, or <Date>}
+         */
+        parseQueryString(date) {
+            if (!date) return undefined;
+            if (date === 'today' || date === 'tomorrow' || date === 'weekend') {
+                return date;
+            }
+            else if (moment(date, ["YYYY-MM-DD"]).isValid()) {
+                return moment(date, ["YYYY-MM-DD"]);
+            }
+        }
 
-                }
+        /**
+         * Formats date to string if passed a date object, otherwise return what is passsed
+         *
+         * @returns {String, or <Date>}
+         */
+        setQueryString(date) {
+            if (!date) return undefined;
+            var dateFormat = wfFormatDateTimeFilter(date, "YYYY-MM-DD");
+            if (dateFormat !== 'Invalid date') {
+                return dateFormat;
+            }
+            else return date;
 
-                /**
-                 * Retrieves an Array of day starts for the localised week.
-                 *
-                 * @returns {Array.<Date>}
-                 */
-                getDaysThisWeek(locationKey) {
-                    var today = this.localiseDateTime(this.now(), locationKey).startOf('day'),
+        }
 
-                        choices = [ moment(today.toDate()) ];
+        /**
+         * Retrieves an Array of day starts for the localised week.
+         *
+         * @returns {Array.<Date>}
+         */
+        getDaysThisWeek(locationKey) {
+            var today = this.localiseDateTime(this.now(), locationKey).startOf('day'),
 
-                    for (var i = 1; i < 7; i++) {
-                        choices.push(moment(today.clone().add(i, 'days').toDate()));
-                    }
+                choices = [ moment(today.toDate()) ];
 
-                    return choices;
-                }
-
-
-                localiseDateTime(dateValue, location) {
-                    location = location || wfLocationService.getCurrentLocationKey();
-
-                    return moment.tz(dateValue, getTimezoneForLocation(location));
-                }
-
+            for (var i = 1; i < 7; i++) {
+                choices.push(moment(today.clone().add(i, 'days').toDate()));
             }
 
-            return new DateParser();
+            return choices;
+        }
+
+
+        localiseDateTime(dateValue, location) {
+            location = location || wfLocationService.getCurrentLocationKey();
+
+            return moment.tz(dateValue, getTimezoneForLocation(location));
+        }
+
+    }
+
+    return new DateParser();
 
         }])
 
