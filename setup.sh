d2031c359b7738185bf48eb7703e272f8399313a
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-05-03 10:58:24 +0100

    move setup to root for old build test

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+red='\x1B[0;31m'
+
+test $(which yarn)
+if [ $? != "0" ]; then
+    echo -e "\n\r\n\r${red}yarn not found: please install yarn from https://yarnpkg.com/${plain}\n\r or run 'npm install -g yarn'"
+    echo -e "Yarn is not required for the application (but is for the scripts)"
+    echo -e "Packages can be manually installed with npm\n\r\n\r"
+
+    exit 1
+fi
+
+printf "\n\rSetting up client side dependancies... \n\r\n\r"
+printf "\n\rInstalling NPM packages via yarn... \n\r\n\r"
+
+yarn
+
+printf "\n\Compiling Javascript... \n\r\n\r"
+
+yarn build
+
+printf "\n\rDone.\n\r\n\r"
