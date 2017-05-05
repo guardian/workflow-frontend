0dc4c55d46b5009a28dc6545772723eb7c1fbd4c
Shaun Dillon <shaun.dillon@guardian.co.uk>
2017-05-05 10:22:16 +0100

    update setup files

--- setup.sh
+++ setup.sh
@@ -2,6 +2,18 @@
 
 red='\x1B[0;31m'
 
+test $(which nvm)
+if [ $? != "0" ]; then
+    echo -e "\n\r\n\r${red}nvm not found: please install nvm from https://github.com/creationix/nvm${plain}\n\r"
+    echo -e "nvm is used to manage multiple versions of node"
+
+    exit 1
+fi
+
+printf "\n\rSwitching to correct version of node using nvm... \n\r\n\r"
+
+nvm use
+
 test $(which yarn)
 if [ $? != "0" ]; then
     echo -e "\n\r\n\r${red}yarn not found: please install yarn from https://yarnpkg.com/${plain}\n\r or run 'npm install -g yarn'"
@@ -16,7 +28,7 @@ printf "\n\rInstalling NPM packages via yarn... \n\r\n\r"
 
 yarn
 
-printf "\n\Compiling Javascript... \n\r\n\r"
+printf "\n\rCompiling Javascript... \n\r\n\r"
 
 yarn build
 
