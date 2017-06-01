bdaa17fa147074e7daf22f710398dd10993181e6
Paul Roberts <paul.roberts@guardian.co.uk>
2017-06-01 12:06:51 +0100

    typo in video maker url

--- app/lib/Atom.scala
+++ app/lib/Atom.scala
@@ -5,5 +5,5 @@ import config.Config
 object Atom {
   lazy val mediaAtomMakerBaseUrl: String = Config.mediaAtomMakerUrl
   lazy val newContentUrl: String = mediaAtomMakerBaseUrl + "/api2/workflow/atoms"
-  lazy val viewContentUrl: String = mediaAtomMakerBaseUrl + "/video/"
+  lazy val viewContentUrl: String = mediaAtomMakerBaseUrl + "/videos/"
 }
