c051d6d7fa9b5771bca903433507dc833ba137a4
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-06-26 11:56:02 +0100

    separate editorial support tables for all stages

--- app/config/Config.scala
+++ app/config/Config.scala
@@ -53,7 +53,7 @@ object Config extends AwsInstanceTags {
 
   lazy val no2faUser: String = "composer.test@guardian.co.uk"
 
-  lazy val editorialSupportDynamoTable: String = "support-staff"
+  lazy val editorialSupportDynamoTable: String = s"support-staff-$stage"
 
   // logstash conf
   private lazy val logStashHost: String = "ingest.logs.gutools.co.uk"
