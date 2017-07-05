f607f2d056a7b656c7c5e5e347a8f1d69060550b
shaundillon <shaun.dillon@guardian.co.uk>
2017-07-05 16:02:37 +0100

    add links for media atoms

--- public/lib/atom-service.js
+++ public/lib/atom-service.js
@@ -39,7 +39,10 @@ function wfAtomService(config) {
     }
 
     function getUrl() {
+      if(atomType === 'media') {
         return `${config.mediaAtomMakerViewAtom}${id || atom.id}`;
+      }
+      return '';
     }
 
     function formatKeywords() {
