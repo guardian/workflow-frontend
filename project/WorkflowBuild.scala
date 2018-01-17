f5209ea8ce53016dac7d04cd87eff46ac9f83668
Paul Roberts <paul.roberts@guardian.co.uk>
2018-01-17 13:25:00 +0000

    experiment with creating a deb package

--- project/WorkflowBuild.scala
+++ project/WorkflowBuild.scala
@@ -7,6 +7,7 @@ import com.typesafe.sbt.web._
 import com.gu.riffraff.artifact.RiffRaffArtifact
 import com.gu.riffraff.artifact.RiffRaffArtifact.autoImport._
 import com.typesafe.sbt.SbtNativePackager._
+import com.typesafe.sbt.packager.debian.JDebPackaging
 import com.typesafe.sbt.packager.Keys._
 import Dependencies._
 import sbtbuildinfo.Plugin._
@@ -53,7 +54,7 @@ object WorkflowBuild extends Build {
  def appDistSettings(application: String, deployJsonDir: Def.Initialize[File] = baseDirectory) = Seq(
     packageName in Universal := application,
     concurrentRestrictions in Universal := List(Tags.limit(Tags.All, 1)),
-    riffRaffPackageType := (packageZipTarball in Universal).value,
+    riffRaffPackageType := (packageBin in Debian).value,
     riffRaffBuildIdentifier := Option(System.getenv("CIRCLE_BUILD_NUM")).getOrElse("dev"),
     riffRaffUploadArtifactBucket := Option("riffraff-artifact"),
     riffRaffUploadManifestBucket := Option("riffraff-builds"),
@@ -61,7 +62,7 @@ object WorkflowBuild extends Build {
     riffRaffPackageName := s"editorial-tools:workflow:$application",
     riffRaffManifestProjectName := riffRaffPackageName.value,
     riffRaffArtifactResources := Seq(
-      riffRaffPackageType.value -> s"$application/$application.tgz",
+      riffRaffPackageType.value -> s"$application/${riffRaffPackageType.value.getName}",
       baseDirectory.value / "conf" / "riff-raff.yaml" -> "riff-raff.yaml"
     ),
     artifactName in Universal := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
@@ -71,6 +72,7 @@ object WorkflowBuild extends Build {
   )
 
   lazy val root = playProject("workflow-frontend")
+                          .enablePlugins(JDebPackaging)
                           .settings(libraryDependencies ++= akkaDependencies ++ awsDependencies ++ googleOAuthDependencies
                                       ++ testDependencies ++ jsonDependencies)
                             .settings(libraryDependencies += filters)
