d14550f9a7854c068401f291a7551075ea4e334a
Michael Barton <michael.barton@guardian.co.uk>
2018-09-27 19:14:27 +0100

    Of course you have to initialise the bouncy castle!

--- notification/src/main/scala/com/gu/workflow/notification/NotificationLambda.scala
+++ notification/src/main/scala/com/gu/workflow/notification/NotificationLambda.scala
@@ -1,8 +1,12 @@
 package com.gu.workflow.notification
 
+import java.security.Security
+
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.gu.workflow.api.SubscriptionsAPI
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class NotificationLambda extends RequestHandler[Unit, Unit] {
@@ -10,6 +14,8 @@ class NotificationLambda extends RequestHandler[Unit, Unit] {
 
   private val s3Client = AmazonS3ClientBuilder.standard().build()
   private val config = NotifierConfig(stage, s3Client)
+
+  Security.addProvider(new BouncyCastleProvider())
 
   private val subsApi = new SubscriptionsAPI(stage, config.webPushPublicKey, config.webPushPrivateKey)
   private val notifier = new Notifier(config.apiUrl, subsApi)
