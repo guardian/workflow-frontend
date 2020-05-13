615170429bcd577425e0146616b9d58dac4bd7e0
Akash Askoolum <akash1810@gmail.com>
2020-05-13 09:18:15 +0100

    add logging for visibility

--- app/lib/AdminPermissionFilter.scala
+++ app/lib/AdminPermissionFilter.scala
@@ -4,6 +4,7 @@ import com.gu.pandomainauth.action.UserRequest
 import com.gu.permissions._
 import config.Config
 import io.circe.Json
+import play.api.Logger
 import play.api.mvc.{ActionFilter, Result, Results}
 
 import scala.concurrent.Future
@@ -18,9 +19,13 @@ object AdminPermissionFilter extends ActionFilter[UserRequest] {
   }
 
   override def filter[A](request:UserRequest[A]): Future[Option[Result]] = Future.successful {
-    if (permissions.hasPermission(adminPermission, request.user.email)) {
+    val email = request.user.email
+
+    if (permissions.hasPermission(adminPermission, email)) {
+      Logger.info(s"User $email has ${adminPermission.name} permissions")
       None
     } else {
+      Logger.warn(s"User $email does not have ${adminPermission.name} permissions")
       Some(Results.Forbidden(views.html.admin.unauthorisedUser(Json.Null)))
     }
   }
