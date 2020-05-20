9eee1381aaef175d2e69bc635f59f39f6b86ce42
Akash Askoolum <akash1810@gmail.com>
2020-05-20 07:54:37 +0100

    update CAPIService to a class

--- app/controllers/CAPIService.scala
+++ app/controllers/CAPIService.scala
@@ -1,23 +1,50 @@
 package controllers
 
-import com.gu.workflow.lib.ContentAPI
-import play.api.mvc.Controller
+import java.net.URI
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.{AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider}
+import com.gu.contentapi.client.IAMSigner
+import com.gu.pandomainauth.PanDomainAuthSettingsRefresher
+import com.gu.workflow.util.AWS
 import config.Config
-import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.ws.WSClient
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
-object CAPIService extends Controller with PanDomainAuthActions{
+class CAPIService(
+  override val config: Config,
+  override val controllerComponents: ControllerComponents,
+  override val wsClient: WSClient,
+  override val panDomainSettings: PanDomainAuthSettingsRefresher
+) extends BaseController with PanDomainAuthActions {
 
-  val contentAPI = new ContentAPI(Config.capiPreviewRole, Config.capiPreviewIamUrl)
+  private val previewSigner = {
+    val capiPreviewCredentials = new AWSCredentialsProviderChain(
+      new ProfileCredentialsProvider("capi"),
+      new STSAssumeRoleSessionCredentialsProvider.Builder(config.capiPreviewRole, "capi").build()
+    )
 
-  def previewCapiProxy(path: String) = APIAuthAction.async { request =>
-
-    import play.api.Play.current
-
-    contentAPI.getPreview(path, request.rawQueryString).map(response =>
-      response.status match {
-        case 200 => Ok(response.json)
-        case _ => BadGateway(s"CAPI returned error code ${response.status}")
-      })
+    new IAMSigner(
+      credentialsProvider = capiPreviewCredentials,
+      awsRegion = AWS.region.getName
+    )
   }
 
+  def previewCapiProxy(path: String): Action[AnyContent] = APIAuthAction.async { request =>
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val url = s"${config.capiPreviewIamUrl}/$path?${request.rawQueryString}"
+    val headers = previewSigner.addIAMHeaders(Map.empty, URI.create(url))
+
+    val req = wsClient
+      .url(url)
+      .withHeaders(headers.toSeq: _*)
+      .get()
+
+    req.map(response => response.status match {
+      case 200 => Ok(response.json)
+      case _ => BadGateway(s"CAPI returned error code ${response.status}")
+    })
+  }
 }
