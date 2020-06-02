bb18905152f73ce5f79d3c00e5cf948de75db492
Akash Askoolum <akash1810@gmail.com>
2020-06-02 16:11:49 +0100

    add tests

---
+++ common-lib/src/test/scala/com/gu/workflow/util/StubDecoratorTest.scala
@@ -0,0 +1,82 @@
+package com.gu.workflow.util
+
+import com.gu.workflow.lib.ContentAPI
+import com.gu.workflow.test.lib.TestData
+import models.{ExternalData, Stub}
+import org.joda.time.LocalDate
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.OneInstancePerTest
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration.{Duration, SECONDS}
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class StubDecoratorTest extends AnyFreeSpec with MockFactory with Matchers with OneInstancePerTest {
+  val initialStub: Stub = TestData.defaultStub().copy(externalData = None)
+
+  val initialExternalData: ExternalData = ExternalData(
+    actualPublicationId = Some(1L),
+    actualBookId = Some(1L),
+    actualBookSectionId = Some(1L),
+    actualNewspaperPageNumber = Some(1),
+    actualNewspaperPublicationDate = Some(LocalDate.now())
+  )
+
+  class MockableContentApi extends ContentAPI("test", "test")
+
+  val contentApi: ContentAPI = stub[MockableContentApi]
+  (contentApi.getTagInternalName (_: Long)(_:ExecutionContext)).when(*, *).returns(Future.successful(Some("internal-tag-name")))
+
+  val decorator = new StubDecorator(contentApi)
+
+  "A stub with actual print information should be decorated" in {
+    val stub = initialStub.copy(externalData = Some(initialExternalData))
+
+    val decoratedStub = Await.result(decorator.withPrintLocationDescriptions(stub), Duration(5, SECONDS))
+    val decoratedExternalData = decoratedStub.externalData.get
+
+    decoratedExternalData.longActualPrintLocationDescription shouldBe Some("internal-tag-name >> internal-tag-name")
+    decoratedExternalData.shortActualPrintLocationDescription shouldBe Some("internal-tag-name")
+
+    decoratedStub.longPlannedPrintLocationDescription shouldBe None
+    decoratedStub.shortPlannedPrintLocationDescription shouldBe None
+  }
+
+  "A stub with planned print information should be decorated" in {
+    val stub = initialStub.copy(
+      plannedPublicationId = Some(1L),
+      plannedBookId = Some(1L),
+      plannedBookSectionId = Some(1L),
+      plannedNewspaperPageNumber = Some(1),
+      plannedNewspaperPublicationDate = Some(LocalDate.now())
+    )
+
+    val decoratedStub = Await.result(decorator.withPrintLocationDescriptions(stub), Duration(5, SECONDS))
+
+    decoratedStub.longPlannedPrintLocationDescription shouldBe Some("internal-tag-name >> internal-tag-name")
+    decoratedStub.shortPlannedPrintLocationDescription shouldBe Some("internal-tag-name")
+
+    decoratedStub.externalData shouldBe None
+  }
+
+  "A stub with both actual and planned print information should be decorated" in {
+    val stub = initialStub.copy(
+      plannedPublicationId = Some(1L),
+      plannedBookId = Some(1L),
+      plannedBookSectionId = Some(1L),
+      plannedNewspaperPageNumber = Some(1),
+      plannedNewspaperPublicationDate = Some(LocalDate.now())
+    ).copy(externalData = Some(initialExternalData))
+
+    val decoratedStub = Await.result(decorator.withPrintLocationDescriptions(stub), Duration(5, SECONDS))
+
+    val decoratedExternalData = decoratedStub.externalData.get
+
+    decoratedExternalData.longActualPrintLocationDescription shouldBe Some("internal-tag-name >> internal-tag-name")
+    decoratedExternalData.shortActualPrintLocationDescription shouldBe Some("internal-tag-name")
+
+    decoratedStub.longPlannedPrintLocationDescription shouldBe Some("internal-tag-name >> internal-tag-name")
+    decoratedStub.shortPlannedPrintLocationDescription shouldBe Some("internal-tag-name")
+  }
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,8 @@ object Dependencies {
     "org.seleniumhq.selenium" % "selenium-java" % "2.35.0" % "test",
     "org.scalatestplus" %% "play" % "1.2.0" % "test",
     "org.apache.httpcomponents" % "httpclient" % "4.5.2",
-    specs2 % Test
+    specs2 % Test,
+    "org.scalamock" %% "scalamock" % "4.4.0" % Test
   )
 
   val logbackDependencies = Seq("net.logstash.logback" % "logstash-logback-encoder" % "6.3")
