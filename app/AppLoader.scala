619b4bf2b723fd118365f0b5ca922d715e8acf35
demetrios_skamiotis <demetrios.skamiotis@guardian.co.uk>
2021-11-22 14:27:01 +0000

    remove logconfig and related from apploader

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,22 +1,10 @@
 import config.Config
-import lib.LogConfig
 import play.api.{Application, ApplicationLoader, LoggerConfigurator}
 
 class AppLoader extends ApplicationLoader {
   override def load(context: ApplicationLoader.Context): Application = {
     LoggerConfigurator(context.environment.classLoader)
       .foreach(_.configure(context.environment))
-
-    val config = new Config(context.initialConfiguration)
-
-    if (config.isDev && config.localLogShipping) {
-      LogConfig.initLocalLogShipping(config.sessionId)
-    }
-
-    (config.loggingStreamName, config.loggingRole) match {
-      case (Some(streamName), Some(role)) => LogConfig.init(config.sessionId, streamName, role)
-      case _ =>
-    }
 
     new AppComponents(context).application
   }
