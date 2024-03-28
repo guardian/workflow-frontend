f94727405b0789e7f3277fabe971975a2634bf02
Rhys Mills <rhys.mills@guardian.co.uk>
2024-03-28 09:28:32 +0000

    Replace single feature switch with multi-switchboard and add Q&A feature switch

--- public/components/feature-switch/feature-switch.js
+++ public/components/feature-switch/feature-switch.js
@@ -6,6 +6,7 @@
 
 import angular from 'angular';
 import featureSwitch from './feature-switch.html';
+import _ from 'lodash';
 
 angular.module('wfFeatureSwitch', ['wfPreferencesService', 'wfIcons'])
     .directive('wfFeatureSwitch', [wfFeatureSwitchDirective]);
@@ -20,13 +21,66 @@ function wfFeatureSwitchDirective() {
     };
 }
 
+
+export const KeyTakeawayFeatureSwitchKey = "keyTakeawaysSwitch";
+export const QAndAFeatureSwitchKey = "qAndASwitch";
+
+class FeatureSwitches {
+    constructor(switches, entries) {
+      this.switches = switches
+      this.entries = entries;
+    }
+
+    update(incomingSwitches) {
+        this.switches = {...this.switches, ...incomingSwitches}
+        this.entries.length = 0;
+        Object.entries(this.switches).forEach(featureSwitch => {
+            this.entries.push(featureSwitch)
+        })
+    }
+}
+
 function wfFeatureSwitchController ($scope, wfPreferencesService) {
+    const featureSwitchKeys = [KeyTakeawayFeatureSwitchKey, QAndAFeatureSwitchKey];
+    $scope.readableNames = {
+        [KeyTakeawayFeatureSwitchKey]: "Show Key Takeaways",
+        [QAndAFeatureSwitchKey]: "Show Q and As",
+    }
 
-    wfPreferencesService.getPreference('featureSwitch').then((data) => { $scope.featureSwitch = data;})
+    const getDefaultFeatureSwitchValues = () => {
+        const switches = {};
+        featureSwitchKeys.forEach(key => switches[key] = false);
+        return switches;
+    }
 
-    $scope.toggleFeatureSwitch = () => {
-        const newValue = !$scope.featureSwitch;
-        wfPreferencesService.setPreference('featureSwitch', newValue).then(setTimeout(()=> {window.location.reload()}, 500)
-       );
-     }
+    $scope.featureSwitchEntries = Object.entries(getDefaultFeatureSwitchValues())
+
+    const featureSwitches = new FeatureSwitches(getDefaultFeatureSwitchValues(), $scope.featureSwitchEntries)
+
+    const updateLocalFeatureSwitchValues = (featureSwitchResult) => {
+        try {
+            const incomingFeatureSwitches = featureSwitchResult;
+            if (_.isObject(incomingFeatureSwitches)){
+                featureSwitches.update(incomingFeatureSwitches);
+                $scope.$apply()
+            }
+        } catch (e) {
+            console.error(`Feature switch JSON is malformed, can't parse. Error message: ${e.message}`);
+            featureSwitches.update(getDefaultFeatureSwitchValues())
+        }
+    }
+
+    wfPreferencesService.getPreference('featureSwitches').then((result) => { 
+        if (result){
+            updateLocalFeatureSwitchValues(result)
+        }
+    })
+    
+    $scope.updateFeatureSwitchPreference = (key, value) => {
+            featureSwitches.update({[key]: value});
+            console.log(featureSwitches.switches)
+            wfPreferencesService.setPreference('featureSwitches', featureSwitches.switches)
+                .then(setTimeout(() => {window.location.reload()}, 500));
+        
+    }
 }
