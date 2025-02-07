72c13878d730a52267554c010da3a771da4043a1
Wai Sing Yiu <89925410+waisingyiu@users.noreply.github.com>
2025-02-07 12:43:12 +0000

    Change the headline toggle icon

--- public/lib/column-defaults.js
+++ public/lib/column-defaults.js
@@ -79,7 +79,7 @@ const createCustomHeadlineLabelHtml = () => {
     return `
         <i ng-click="toggleHeadlineDisplay(); $event.stopPropagation()"
            title="Click to toggle Headlines"
-           class="content-list-head__heading-icon--headline-toggle" wf-icon="toggle"/>
+           class="content-list-head__heading-icon--headline-toggle" wf-icon="{{ getHeadlineDisplay()? 'toggle-right': 'toggle-left' }}"/>
         <span ng-class="{'content-list-head__heading--titles--secondary': getHeadlineDisplay()}">Working title</span> / <span ng-class="{ 'content-list-head__heading--titles--secondary': !getHeadlineDisplay()}">Headline</span>
     `;
 };
