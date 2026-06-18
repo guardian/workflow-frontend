0ef85462d214942618b43c5a2213ce20f773567c
Liam Duffy <3338808+liamness@users.noreply.github.com>
2026-06-18 16:03:58 +0100

    Adjust import and usage of CloseableHttpResponse

--- common-lib/src/test/scala/lib/Response.scala
+++ common-lib/src/test/scala/lib/Response.scala
@@ -1,13 +1,13 @@
 package com.gu.workflow.test
 
-import org.apache.http.client.methods.{CloseableHttpResponse}
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse
 import scala.io.Source
 
 
 class Response(val response: CloseableHttpResponse) {
   lazy val body = Source.fromInputStream(response.getEntity.getContent).getLines().mkString("")
-  lazy val responseCode = response.getStatusLine.getStatusCode
-  lazy val responseMessage = response.getStatusLine.getReasonPhrase
+  lazy val responseCode = response.getCode()
+  lazy val responseMessage = response.getReasonPhrase()
 
   def header(name: String) = response.getFirstHeader(name).getValue
 
