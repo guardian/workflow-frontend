50fb528bfc1daa7369bc7f6041d1d31f7bf23b9b
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2026-06-23 12:33:46 +0100

    use tracking tags to display intended audience.

--- public/react/IntendedAudienceWrapper.tsx
+++ public/react/IntendedAudienceWrapper.tsx
@@ -5,28 +5,23 @@ import {
 import React from "react";
 
 type Props = {
-  intendedAudience?: string;
+  trackingTags?: string[];
 };
 
-const getSourceAndTarget = (intendedAudience?: string) => {
-  if (!intendedAudience) {
+const getSourceAndTarget = (trackingTags?: string[]) => {
+  if (!trackingTags) {
     return undefined;
   }
 
-  // We intend to deprecate the stub.externalData.intendedAudience (Option[String], comma separated tag slugs) and use
-  // stub.externalData.trackingTags (Option[List[String]], array of tag paths)
-
   return mapTagsToSourceAndTarget(
-    intendedAudience.toLowerCase()
-      .split(",")
-      .map((slug) => ({ path: `tracking/audience/${slug}` })),
+    trackingTags.map((path) => ({ path })),
   );
 };
 
 export const IntendedAudienceWrapper: React.FunctionComponent<Props> = ({
-  intendedAudience,
+  trackingTags,
 }: Props) => {
-  const sourceAndTarget = getSourceAndTarget(intendedAudience);
+  const sourceAndTarget = getSourceAndTarget(trackingTags);
   if (!sourceAndTarget) {
     return null; // do not render the "Don't know" signifier in the table
   }
