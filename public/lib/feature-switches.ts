336d4f789e9bc2a39f761a5f840b8a70ec51ae7b
DBlatcher <david.blatcher@guardian.co.uk>
2026-07-01 12:08:31 +0100

    add some typing for the feature switches to allow defaultValue to be true and define the description. Make "intendedAudienceColumn" true by default

--- public/lib/feature-switches.ts
+++ public/lib/feature-switches.ts
@@ -1,13 +1,33 @@
-
 type FeatureSwitchData = Record<string, unknown>;
 
-export const featureSwitchKeys = [
-  // e.g. 'multiByline'
-  "intendedAudienceColumn",
+type FeatureSwitch = {
+  key: string;
+  defaultValue: boolean;
+  description: string;
+};
+
+const featureSwitchList: FeatureSwitch[] = [
+  {
+    key: "intendedAudienceColumn",
+    defaultValue: true,
+    description: "Show Intended Audience",
+  },
 ];
 
-export const getDefaultFeatureSwitchValues = ():FeatureSwitchData => {
-  const switches:FeatureSwitchData = {};
-  featureSwitchKeys.forEach((key) => (switches[key] = false));
+export const featureSwitchKeys = featureSwitchList.map(
+  (featureSwitch) => featureSwitch.key,
+);
+
+export const getDefaultFeatureSwitchValues = (): FeatureSwitchData => {
+  const switches: FeatureSwitchData = {};
+  featureSwitchList.forEach(
+    ({ key, defaultValue }) => (switches[key] = defaultValue),
+  );
   return switches;
 };
+
+export const featureSwitchReadableNames = featureSwitchList.reduce<
+  Record<string, string>
+>((namesMap, nextSwitch) => {
+  return { ...namesMap, [nextSwitch.key]: nextSwitch.description };
+}, {});
