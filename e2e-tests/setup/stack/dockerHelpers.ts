98ce55735b07837064802038854e5eb3160c945d
Wai Sing Yiu <89925410+waisingyiu@users.noreply.github.com>
2026-09-09 09:25:52 +0000

    Refactoring

--- e2e-tests/setup/stack/dockerHelpers.ts
+++ e2e-tests/setup/stack/dockerHelpers.ts
@@ -11,7 +11,7 @@ export function buildImage(
     console.log(`\n[docker-build] Building ${tag} from ${dockerfileName}`);
     return GenericContainer.fromDockerfile(context, dockerfileName)
         .withBuildkit()
-        .build(tag, { deleteOnExit: false });
+        .build(tag, { deleteOnExit: true });
 }
 
 export function createLogConsumer(prefix: string, streamLogs: boolean) {
